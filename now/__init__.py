89362f4afbe5b8c3317d8266f66355eb226b1095
Mohammad Kalim Akram <kalim.akram@jina.ai>
2022-05-18 12:08:49 +0200

    chore: bump version

--- now/__init__.py
+++ now/__init__.py
@@ -1,7 +1,8 @@
+__version__ = '0.0.6'
+
 import os as _os
 import sys as _sys
 
-__version__ = '0.0.5'
 __resources_path__ = _os.path.join(
     _os.path.dirname(_sys.modules['now'].__file__), 'resources'
 )
