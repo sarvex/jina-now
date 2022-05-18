80d3fb77b3812a5270673bccfbf90ecf108170fa
Mohammad Kalim Akram <kalimakram@gmail.com>
2022-05-18 11:39:24 +0200

    fix(ci): add dependency in cd (#175)
    * fix(ci): add dependency in cd
    
    * fix: install ffmpeg
    
    * fix: no codecov token
    
    * fix: revert the need for tests
    
    * fix: revert this change later
    
    * fix: reverted release change
    
    * fix: add time estimate for integ test
    
    * fix: add time estimate in min and sec
    
    * fix: revert the batch size

--- tests/integration/test_end_to_end.py
+++ tests/integration/test_end_to_end.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 from argparse import Namespace
 from os.path import expanduser as user
 
@@ -15,6 +16,7 @@ from now.run_all_k8s import get_remote_flow_details
 
 @pytest.fixture()
 def cleanup(deployment_type, dataset):
+    start = time.time()
     yield
     if deployment_type == 'remote':
         if dataset == 'best-artworks':
@@ -28,11 +30,23 @@ def cleanup(deployment_type, dataset):
         }
         kwargs = Namespace(**kwargs)
         cli(args=kwargs)
+    now = time.time() - start
+    mins = int(now / 60)
+    secs = int(now % 60)
+    print(50 * '#')
+    print(
+        f'Time taken to execute `{deployment_type}` deployment with dataset `{dataset}`: {mins}m {secs}s'
+    )
+    print(50 * '#')
 
 
 @pytest.mark.parametrize(
     'output_modality, dataset',
-    [('image', 'bird-species'), ('image', 'best-artworks'), ('text', 'rock-lyrics')],
+    [
+        ('image', 'bird-species'),
+        ('image', 'best-artworks'),
+        ('text', 'rock-lyrics'),
+    ],
 )  # art, rock-lyrics -> no finetuning, fashion -> finetuning
 @pytest.mark.parametrize('quality', ['medium'])
 @pytest.mark.parametrize('cluster', [NEW_CLUSTER['value']])
