f5b9499b57a3979158300c0da6068db8445132d6
slettner <sebastian.lettner@jina.ai>
2022-06-07 20:44:32 +0200

    chore: migrate sample datasets

--- tests/integration/data_loading/test_demo_datasets.py
+++ tests/integration/data_loading/test_demo_datasets.py
@@ -2,9 +2,10 @@
 This suite tests that the demo datasets are available
 behind their URLs.
 """
-
+import docarray
 import pytest
 import requests
+from starlette import status
 
 from now.constants import AVAILABLE_DATASET, Modalities, Qualities
 from now.data_loading.data_loading import get_dataset_url
@@ -26,4 +27,28 @@ def test_dataset_is_available(
         pytest.skip('music case not fully implemented yet')
     url = get_dataset_url(ds_name, quality, modality)
 
-    assert requests.head(url).status_code == 200
+    assert requests.head(url).status_code == status.HTTP_200_OK
+
+
+@pytest.mark.parametrize(
+    'ds_url',
+    [
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/jpeg/nft-monkey.img10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/jpeg/deepfashion.img10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/jpeg/nih-chest-xrays.img10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/jpeg/stanford-cars.img10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/jpeg/bird-species.img10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/jpeg/best-artworks.img10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/jpeg/geolocation-geoguessr.img10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/text/rock-lyrics.txt10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/text/pop-lyrics.txt10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/text/rap-lyrics.txt10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/text/indie-lyrics.txt10-{}.bin",
+        "https://storage.googleapis.com/jina-fashion-data/data/one-line/datasets/text/metal-lyrics.txt10-{}.bin",
+    ],
+)
+def test_sample_data_is_available(ds_url: str):
+    assert (
+        requests.head(url=ds_url.format(docarray.__version__)).status_code
+        == status.HTTP_200_OK
+    )
