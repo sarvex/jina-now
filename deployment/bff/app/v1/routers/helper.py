f5c1be452495b428bd426ca65c60a513da69fdbe
Sebastian Lettner <51201318+slettner@users.noreply.github.com>
2022-06-09 10:32:33 +0200

    feat: update bff music (#243)
    * feat: add music endpoints
    
    * test(bff): add dependecy override
    
    * chore: update docker tags
    
    * fix(bff): dep injection
    
    * chore: update docker tag

--- deployment/bff/app/v1/routers/helper.py
+++ deployment/bff/app/v1/routers/helper.py
@@ -2,18 +2,19 @@ import base64
 
 from docarray import Document
 from fastapi import HTTPException
+from jina import Client
 
 
-def process_query(text: str, image: str) -> Document:
-    if text is None and image is None:
-        raise ValueError('Please set one of the value - `image` or `text`')
-    if text is not None and image is not None:
+def process_query(text: str, blob: str) -> Document:
+    if text is None and blob is None:
+        raise ValueError('Please set one of the value - `blob` or `text`')
+    if text is not None and blob is not None:
         raise ValueError('Please set either image or text not both!')
     try:
         if text is not None:
             query_doc = Document(text=text)
         else:
-            base64_bytes = image.encode('utf-8')
+            base64_bytes = blob.encode('utf-8')
             message_bytes = base64.decodebytes(base64_bytes)
             query_doc = Document(blob=message_bytes)
     except BaseException as e:
@@ -22,3 +23,10 @@ def process_query(text: str, image: str) -> Document:
             detail=f'Not a correct encoded query. Please see the error stack for more information. \n{e}',
         )
     return query_doc
+
+
+def get_jina_client(host: str, port: int) -> Client:
+    if 'wolf.jina.ai' in host:
+        return Client(host=host)
+    else:
+        return Client(host=host, port=port)
--- tests/unit/bff/v1/routers/test_text.py
+++ tests/unit/bff/v1/routers/test_text.py
@@ -1,21 +1,34 @@
+from typing import Callable
+
 import pytest
+import requests
+from docarray import Document, DocumentArray
+from starlette import status
 
 
-def test_index(test_client, test_index_text):
+@pytest.fixture
+def sample_search_response_text() -> DocumentArray:
+    result = DocumentArray([Document()])
+    matches = DocumentArray([Document(text='match')])
+    result[0].matches = matches
+    return result
+
+
+def test_text_index_fails_with_no_flow_running(client: requests.Session):
     with pytest.raises(ConnectionError):
-        test_client.post(f'/api/v1/text/index', json=test_index_text)
+        client.post(f'/api/v1/text/index', json={'texts': ['Hello']})
 
 
-def test_search(test_client, test_search_text):
+def test_text_search_fails_with_no_flow_running(client: requests.Session):
     with pytest.raises(ConnectionError):
-        test_client.post(
+        client.post(
             f'/api/v1/text/search',
-            json=test_search_text,
+            json={'text': 'Hello'},
         )
 
 
-def test_search_text_via_no_base64_image(test_client):
-    response = test_client.post(
+def test_text_search_fails_with_incorrect_query(client):
+    response = client.post(
         f'/api/v1/text/search',
         json={'image': 'hello'},
     )
@@ -23,25 +36,47 @@ def test_search_text_via_no_base64_image(test_client):
     assert 'Not a correct encoded query' in response.text
 
 
-def test_search_text_via_base64_image(test_client, test_search_image):
-    with pytest.raises(ConnectionError):
-        test_client.post(
-            f'/api/v1/text/search',
-            json=test_search_image,
-        )
-
-
-def test_no_query(test_client):
+def test_text_search_fails_with_emtpy_query(client: requests.Session):
     with pytest.raises(ValueError):
-        test_client.post(
+        client.post(
             f'/api/v1/text/search',
             json={},
         )
 
 
-def test_both_query(test_client, test_search_both):
-    with pytest.raises(ValueError):
-        test_client.post(
-            f'/api/v1/text/search',
-            json=test_search_both,
-        )
+def test_text_index(
+    client_with_mocked_jina_client: Callable[[DocumentArray], requests.Session],
+):
+    response = client_with_mocked_jina_client(DocumentArray()).post(
+        '/api/v1/text/index', json={'texts': ['Hello']}
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_text_search_calls_flow(
+    client_with_mocked_jina_client: Callable[[DocumentArray], requests.Session],
+    sample_search_response_text: DocumentArray,
+):
+    response = client_with_mocked_jina_client(sample_search_response_text).post(
+        '/api/v1/text/search', json={'text': 'Hello'}
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    results = DocumentArray.from_json(response.content)
+    # the mock writes the call args into the response tags
+    assert results[0].tags['url'] == '/search'
+    assert results[0].tags['parameters']['limit'] == 10
+
+
+def test_text_search_parse_response(
+    client_with_mocked_jina_client: Callable[[DocumentArray], requests.Session],
+    sample_search_response_text: DocumentArray,
+):
+    response = client_with_mocked_jina_client(sample_search_response_text).post(
+        '/api/v1/text/search', json={'text': 'Hello'}
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    results = DocumentArray.from_json(response.content)
+    assert len(results) == len(sample_search_response_text[0].matches)
+    assert results[0].text == sample_search_response_text[0].matches[0].text
