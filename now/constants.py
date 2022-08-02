90e8fa7a5eadea6d9826f6ba74db9f94c4030598
Florian Hönicke <hoenicke.florian@gmail.com>
2022-08-03 00:50:06 +0200

    refactor: bump playground version

--- now/constants.py
+++ now/constants.py
@@ -1,7 +1,7 @@
 from now.utils import BetterEnum
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.86'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.87'
 
 
 class Modalities(BetterEnum):
