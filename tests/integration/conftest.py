6d564c9ffee2f7a2328d6e990d89d48f10a33302
Florian Hönicke <hoenicke.florian@gmail.com>
2022-08-12 15:38:08 +0200

    fix: hubble login call test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -6,26 +6,25 @@ from pytest_mock import MockerFixture
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
-#
-#
-# class HubbleAuthPatch:
-#     @staticmethod
-#     async def login():
-#         pass
-#
-#     @staticmethod
-#     def get_auth_token() -> str:
-#         token = os.environ.get('WOLF_TOKEN')
-#         if token:
-#             log.debug(f'Found token in env *** (Len={len(token)})')
-#             return token
-#         else:
-#             raise RuntimeError(
-#                 'WOLF token not found in environment under key `WOLF_TOKEN`'
-#             )
+
+
+class HubbleAuthPatch:
+    @staticmethod
+    async def login():
+        pass
+
+    @staticmethod
+    def get_auth_token() -> str:
+        token = os.environ.get('WOLF_TOKEN')
+        if token:
+            log.debug(f'Found token in env *** (Len={len(token)})')
+            return token
+        else:
+            raise RuntimeError(
+                'WOLF token not found in environment under key `WOLF_TOKEN`'
+            )
 
 
 @pytest.fixture
 def with_hubble_login_patch(mocker: MockerFixture) -> None:
-    os.environ['JINA_AUTH_TOKEN'] = os.environ.get('WOLF_TOKEN')
-    # mocker.patch(target='finetuner.client.base.hubble.Auth', new=HubbleAuthPatch)
+    mocker.patch(target='finetuner.client.base.hubble.Auth', new=HubbleAuthPatch)
