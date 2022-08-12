c24588920a76fd67ca06b9b48afc19e7ef70ffa3
Florian Hönicke <hoenicke.florian@gmail.com>
2022-08-12 18:42:06 +0200

    fix: version hubble (#337)
    * fix: version hubble
    
    * fix: no sleep
    
    * fix: wolf json
    
    * refactor: use token directly
    
    * fix: path
    
    * fix: creat folders
    
    * fix: path
    
    * fix: hubble login call test
    
    * fix: sha
    
    * fix: ci
    
    * fix: wolf json
    
    * fix: tag
    
    * fix: bump version
    
    * fix: not waiting
    
    * test: token exists

--- now/constants.py
+++ now/constants.py
@@ -5,7 +5,7 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.98'
-NOW_PREPROCESSOR_VERSION = '0.0.23'
+NOW_PREPROCESSOR_VERSION = '0.0.30'
 
 
 class Modalities(BetterEnum):
