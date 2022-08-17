eb2efd696252f9251e0c94f351c8b95e8a1786b9
Florian Hönicke <hoenicke.florian@gmail.com>
2022-08-17 21:57:29 +0200

    feat: api key (#361)
    * feat: api key
    
    * refactor: change naming
    
    * refactor: bump version

--- deployment/bff/app/v1/routers/admin.py
+++ deployment/bff/app/v1/routers/admin.py
@@ -1,13 +1,15 @@
 from docarray import Document
 from fastapi import APIRouter
 
-from deployment.bff.app.v1.models.admin import UpdateEmailsRequestModel
+from deployment.bff.app.v1.models.admin import (
+    UpdateApiKeysRequestModel,
+    UpdateEmailsRequestModel,
+)
 from deployment.bff.app.v1.routers.helper import jina_client_post
 
 router = APIRouter()
 
 
-# Index
 @router.post(
     "/updateUserEmails",
     summary='update user emails during runtime',
@@ -22,4 +24,21 @@ def update_user_email(data: UpdateEmailsRequestModel):
         inputs=[Document()],
         endpoint='/admin/updateUserEmails',
         parameters={'jwt': data.jwt, 'user_emails': data.user_emails},
+    )
+
+
+@router.post(
+    "/updateApiKeys",
+    summary='update api keys during runtime',
+)
+def update_user_email(data: UpdateApiKeysRequestModel):
+    """
+    Update the list of api keys for the security executor
+    """
+    jina_client_post(
+        host=data.host,
+        port=data.port,
+        inputs=[Document()],
+        endpoint='/admin/updateApiKeys',
+        parameters={'jwt': data.jwt, 'api_keys': data.api_keys},
     )
