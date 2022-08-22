39ff5f05d0cabd46c212b8407c384f0fe5577b21
Mohammad Kalim Akram <kalim.akram@jina.ai>
2022-08-22 22:55:49 +0200

    fix: reverse version

--- now/constants.py
+++ now/constants.py
@@ -5,7 +5,7 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.110-increase-limit-1'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.110-add-webcam'
 NOW_PREPROCESSOR_VERSION = '0.0.49-annlite-chunks'
 NOW_AUTH_EXECUTOR_VERSION = '0.0.3'
 
