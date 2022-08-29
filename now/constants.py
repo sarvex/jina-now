22927c1e18382827d1252549cda991e03c0f27d5
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-08-29 13:02:03 +0100

    refactor: loading tags

--- now/constants.py
+++ now/constants.py
@@ -5,8 +5,8 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.113-gif-demo-data-7'
-NOW_PREPROCESSOR_VERSION = '0.0.61-gif-demo-data-7'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.113-refactor-tags'
+NOW_PREPROCESSOR_VERSION = '0.0.61-refactor-tags'
 NOW_AUTH_EXECUTOR_VERSION = '0.0.3'
 
 
