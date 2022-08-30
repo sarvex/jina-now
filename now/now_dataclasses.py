2619de5cc1287521012db9b1f8a3f006c78b4d14
jupyterjazz <saba.sturua@jina.ai>
2022-08-30 16:12:39 +0200

    feat: add data classes

--- now/now_dataclasses.py
+++ now/now_dataclasses.py
@@ -1,3 +1,4 @@
+from pydantic.dataclasses import dataclass as pydantic_dataclass
 from dataclasses import dataclass
 from typing import Dict, List, Optional
 
@@ -33,3 +34,33 @@ class UserInput:
     admin_emails: Optional[List[str]] = None
     user_emails: Optional[List[str]] = None
     additional_user: Optional[bool] = False
+
+
+@pydantic_dataclass
+class TrainDataGeneratorConfig:
+    method: str
+    parameters: Dict
+    scope: List[str]
+
+
+@pydantic_dataclass
+class TrainDataGenerationConfig:
+    query: TrainDataGeneratorConfig
+    target: TrainDataGeneratorConfig
+
+
+@pydantic_dataclass
+class EncoderConfig:
+    name: str
+    encoder_type: str
+    target_fields: List[str]
+    train_dataset_name: str
+    training_data_generation_methods: List[TrainDataGenerationConfig]
+
+
+@pydantic_dataclass
+class Task:
+    name: str
+    extracted_dataset: str
+    encoders: List[EncoderConfig]
+    add_indexer: bool
