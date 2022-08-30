a24c9195085a44d0fa534b8e131af3eae4628133
jupyterjazz <saba.sturua@jina.ai>
2022-08-30 16:40:57 +0200

    feat: add indexer scope

--- now/now_dataclasses.py
+++ now/now_dataclasses.py
@@ -1,45 +1,14 @@
 from pydantic.dataclasses import dataclass as pydantic_dataclass
 from dataclasses import dataclass
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Any
 
 from now.constants import Apps, DatasetTypes, Qualities
-
-
-@dataclass
-class UserInput:
-    app: Optional[Apps] = None
-
-    # data related
-    data: Optional[str] = None
-    is_custom_dataset: Optional[bool] = None
-
-    custom_dataset_type: Optional[DatasetTypes] = None
-    dataset_name: Optional[str] = None
-    dataset_url: Optional[str] = None
-    dataset_path: Optional[str] = None
-    # AWS related
-    aws_access_key_id: Optional[str] = None
-    aws_secret_access_key: Optional[str] = None
-    aws_region_name: Optional[str] = None
-
-    # model related
-    quality: Optional[Qualities] = Qualities.MEDIUM
-
-    # cluster related
-    cluster: Optional[str] = None
-    create_new_cluster: Optional[bool] = False
-    deployment_type: Optional[str] = None
-    secured: Optional[bool] = False
-    jwt: Optional[Dict[str, str]] = None
-    admin_emails: Optional[List[str]] = None
-    user_emails: Optional[List[str]] = None
-    additional_user: Optional[bool] = False
 
 
 @pydantic_dataclass
 class TrainDataGeneratorConfig:
     method: str
-    parameters: Dict
+    parameters: Dict[str, Any]
     scope: List[str]
 
 
@@ -63,4 +32,38 @@ class Task:
     name: str
     extracted_dataset: str
     encoders: List[EncoderConfig]
-    add_indexer: bool
+    indexer_scope: Dict[str, str]
+
+
+@dataclass
+class UserInput:
+    app: Optional[Apps] = None
+
+    # data related
+    data: Optional[str] = None
+    is_custom_dataset: Optional[bool] = None
+
+    custom_dataset_type: Optional[DatasetTypes] = None
+    dataset_name: Optional[str] = None
+    dataset_url: Optional[str] = None
+    dataset_path: Optional[str] = None
+    # AWS related
+    aws_access_key_id: Optional[str] = None
+    aws_secret_access_key: Optional[str] = None
+    aws_region_name: Optional[str] = None
+
+    # ES related
+    task_config: Optional[Task] = None
+
+    # model related
+    quality: Optional[Qualities] = Qualities.MEDIUM
+
+    # cluster related
+    cluster: Optional[str] = None
+    create_new_cluster: Optional[bool] = False
+    deployment_type: Optional[str] = None
+    secured: Optional[bool] = False
+    jwt: Optional[Dict[str, str]] = None
+    admin_emails: Optional[List[str]] = None
+    user_emails: Optional[List[str]] = None
+    additional_user: Optional[bool] = False
