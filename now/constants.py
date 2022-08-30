3443616415bc4eb7a9164cbd4d5ee6a64cd7d415
Florian Hönicke <hoenicke.florian@gmail.com>
2022-08-30 17:19:18 +0200

    refactor: bump version

--- now/constants.py
+++ now/constants.py
@@ -5,8 +5,8 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.114-newplaygorund'
-NOW_PREPROCESSOR_VERSION = '0.0.64-retention-days'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.114-newplaygorund-2'
+NOW_PREPROCESSOR_VERSION = '0.0.64-newpreprocessor-2'
 
 NOW_AUTH_EXECUTOR_VERSION = '0.0.3'
 
