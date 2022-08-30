34293236981bf0a6ee75d0bf47fe033e7753fae0
Florian Hönicke <hoenicke.florian@gmail.com>
2022-08-30 18:00:16 +0200

    feat: add path preprocessor (#411)

--- now/constants.py
+++ now/constants.py
@@ -5,8 +5,9 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
+
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.114-newplaygorund-2'
-NOW_PREPROCESSOR_VERSION = '0.0.64-newpreprocessor-2'
+NOW_PREPROCESSOR_VERSION = '0.0.65-feat-add-path-preprocessor'
 
 NOW_AUTH_EXECUTOR_VERSION = '0.0.3'
 
