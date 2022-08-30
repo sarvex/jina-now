83e9dd2edce92cc02f26810546281c23a206bd83
Joschka Braun <joschka.braun@gmail.com>
2022-08-30 11:23:51 +0200

    fix: reset post processor mem usage

--- now/constants.py
+++ now/constants.py
@@ -6,7 +6,7 @@ from now.utils import BetterEnum
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.113-feat-client-6'
-NOW_PREPROCESSOR_VERSION = '0.0.63-add-a-flow'
+NOW_PREPROCESSOR_VERSION = '0.0.63-postprocessor-memory'
 NOW_AUTH_EXECUTOR_VERSION = '0.0.3'
 
 
