a324a17ab92bb4c60173dd736c7409797abcb143
Florian Hönicke <hoenicke.florian@gmail.com>
2022-09-05 12:52:22 +0200

    refactor: bump jina version (#464)
    * refactor: bump jina version
    
    * refactor: bump jina version

--- now/constants.py
+++ now/constants.py
@@ -5,7 +5,7 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.116-use-cas-2'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.117-refactor-bump-jina-version'
 NOW_PREPROCESSOR_VERSION = '0.0.72-refactor-ci-preprocessor'
 
 NOW_AUTH_EXECUTOR_VERSION = '0.0.1'
