8ea9ec4733fdffd7ee42aab5bc864521afe98b61
Joschka Braun <47435119+joschkabraun@users.noreply.github.com>
2022-09-05 13:18:49 +0200

    Fix auth exec restart (#461)
    * feat: move all executors into public
    
    * fix: building pushing preprocessor
    
    * chore: bump version
    
    * fix: remove gpu
    
    * fix: remove gpu
    
    * fix: new auth executor
    
    * fix: add key test
    
    * fix: old preprocessor
    
    * fix: add api key test
    
    * fix: auth exec restart issue
    
    * fix: change ci
    
    * fix: typo
    
    * fix: another typo
    
    * fix: yet another typo
    
    * chore: bump version
    
    * chore: bump version
    
    * chore: bump version
    
    * fix: fix jina version in ci
    
    * test: write tests
    
    * fix: update jina v for preprocessor build
    
    * chore: bump version
    
    * chore: bump version
    
    * fix: typo
    
    * chore: bump version

--- now/constants.py
+++ now/constants.py
@@ -6,9 +6,9 @@ from now.utils import BetterEnum
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.117-refactor-bump-jina-version'
-NOW_PREPROCESSOR_VERSION = '0.0.72-refactor-ci-preprocessor'
+NOW_PREPROCESSOR_VERSION = '0.0.72-auth-exec-13'
 
-NOW_AUTH_EXECUTOR_VERSION = '0.0.1'
+NOW_AUTH_EXECUTOR_VERSION = '0.0.3-8'
 
 
 class Modalities(BetterEnum):
