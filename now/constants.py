738020fb165e1f76176fd4313445dec8f36df557
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-09-13 09:07:50 +0100

    fix: tags

--- now/constants.py
+++ now/constants.py
@@ -5,10 +5,10 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.123-adapt-playground-10'
-NOW_PREPROCESSOR_VERSION = '0.0.83-finetuning6'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.123-adapt-playground-11'
+NOW_PREPROCESSOR_VERSION = '0.0.83-adapt-playground-1'
 NOW_AUTH_EXECUTOR_VERSION = '0.0.5-auth-exec-4'
-NOW_ANNLITE_INDEXER_VERSION = '0.0.3-annlite-adapt-playground-7'
+NOW_ANNLITE_INDEXER_VERSION = '0.0.3-annlite-adapt-playground-8'
 
 
 class Modalities(BetterEnum):
