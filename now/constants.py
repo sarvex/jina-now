963189b935790765a4bc7b971f5ec50d2ecc22d1
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-09-23 18:13:13 +0100

    fix: requirements.txt

--- now/constants.py
+++ now/constants.py
@@ -5,9 +5,9 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.126-playground-secured-tags-16'
-NOW_PREPROCESSOR_VERSION = '0.0.86-playground-secured-tags-16'
-NOW_ANNLITE_INDEXER_VERSION = '0.0.5-annlite-playground-secured-tags-16'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.126-playground-secured-tags-17'
+NOW_PREPROCESSOR_VERSION = '0.0.86-playground-secured-tags-17'
+NOW_ANNLITE_INDEXER_VERSION = '0.0.5-annlite-playground-secured-tags-17'
 
 
 class Modalities(BetterEnum):
