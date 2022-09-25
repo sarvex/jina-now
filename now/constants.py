c444e85ff663ab3a3d292e35baeb265226b766e1
Florian Hönicke <hoenicke.florian@gmail.com>
2022-09-25 15:11:39 +0200

    refactor: request size

--- now/constants.py
+++ now/constants.py
@@ -5,7 +5,7 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.126-playground-demo-url-2'
-NOW_PREPROCESSOR_VERSION = '0.0.87-ci-executors-tests-18'
+NOW_PREPROCESSOR_VERSION = '0.0.88-refactor-request-size-1'
 NOW_ANNLITE_INDEXER_VERSION = '0.0.5-ci-executors-tests-18'
 
 
