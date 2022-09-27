174013415dd3561d6188f5640fc0b5cb7eedaa09
Joschka Braun <joschka.braun@gmail.com>
2022-09-27 16:07:39 +0200

    refactor: embed demo dataset

--- now_common/options.py
+++ now_common/options.py
@@ -75,10 +75,11 @@ DATA = DialogOptions(
     ),
 )
 
-CUSTOM_DATASET_TYPE = DialogOptions(
+DATASET_TYPE = DialogOptions(
     name='custom_dataset_type',
     prompt_message='How do you want to provide input? (format: https://docarray.jina.ai/)',
     choices=[
+        {'name': 'Demo dataset', 'value': DatasetTypes.DEMO},
         {
             'name': 'DocArray name (recommended)',
             'value': DatasetTypes.DOCARRAY,
@@ -110,7 +111,7 @@ DATASET_NAME = DialogOptions(
     name='dataset_name',
     prompt_message='Please enter your DocArray name:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.DOCARRAY,
 )
@@ -119,7 +120,7 @@ DATASET_URL = DialogOptions(
     name='dataset_url',
     prompt_message='Please paste in the URL to download your DocArray from:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.URL,
 )
@@ -128,7 +129,7 @@ DATASET_PATH = DialogOptions(
     name='dataset_path',
     prompt_message='Please enter the path to the local folder:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.PATH,
 )
@@ -137,7 +138,7 @@ DATASET_PATH_S3 = DialogOptions(
     name='dataset_path',
     prompt_message='Please enter the S3 URI to the folder:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.S3_BUCKET,
 )
@@ -146,7 +147,7 @@ AWS_ACCESS_KEY_ID = DialogOptions(
     name='aws_access_key_id',
     prompt_message='Please enter the AWS access key ID:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.S3_BUCKET,
 )
@@ -155,7 +156,7 @@ AWS_SECRET_ACCESS_KEY = DialogOptions(
     name='aws_secret_access_key',
     prompt_message='Please enter the AWS secret access key:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.S3_BUCKET,
 )
@@ -164,7 +165,7 @@ AWS_REGION_NAME = DialogOptions(
     name='aws_region_name',
     prompt_message='Please enter the AWS region:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.S3_BUCKET,
 )
@@ -175,7 +176,7 @@ ES_TEXT_FIELDS = DialogOptions(
     name='es_text_fields',
     prompt_message='Please enter comma-separated text fields of your data:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.ELASTICSEARCH,
     post_func=lambda user_input, **kwargs: _parse_text_fields(user_input),
@@ -192,7 +193,7 @@ ES_IMAGE_FIELDS = DialogOptions(
     name='es_image_fields',
     prompt_message='Please enter comma-separated image fields of your data:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input, **kwargs: user_input.custom_dataset_type
     == DatasetTypes.ELASTICSEARCH,
     post_func=lambda user_input, **kwargs: _parse_image_fields(user_input),
@@ -209,7 +210,7 @@ ES_INDEX_NAME = DialogOptions(
     name='es_index_name',
     prompt_message='Please enter the name of your Elasticsearch index:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.ELASTICSEARCH,
 )
@@ -218,7 +219,7 @@ ES_HOST_NAME = DialogOptions(
     name='es_host_name',
     prompt_message='Please enter the address of your Elasticsearch node:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.ELASTICSEARCH,
 )
@@ -227,7 +228,7 @@ ES_ADDITIONAL_ARGS = DialogOptions(
     name='es_additional_args',
     prompt_message='Please enter additional arguments for your Elasticsearch node if there are any:',
     prompt_type='input',
-    depends_on=CUSTOM_DATASET_TYPE,
+    depends_on=DATASET_TYPE,
     conditional_check=lambda user_input: user_input.custom_dataset_type
     == DatasetTypes.ELASTICSEARCH,
 )
@@ -424,7 +425,7 @@ def _parse_custom_data_from_cli(user_input: UserInput, **kwargs) -> None:
 
 
 data = [DATA]
-data_da = [CUSTOM_DATASET_TYPE, DATASET_NAME, DATASET_PATH, DATASET_URL]
+data_da = [DATASET_TYPE, DATASET_NAME, DATASET_PATH, DATASET_URL]
 data_s3 = [DATASET_PATH_S3, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION_NAME]
 data_es = [
     ES_HOST_NAME,
