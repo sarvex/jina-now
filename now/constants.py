ae71db24575b6eb633b692132ad16c3dd6a7d9c5
Joschka Braun <joschka.braun@gmail.com>
2022-09-27 21:01:15 +0200

    chore: bump version

--- now/constants.py
+++ now/constants.py
@@ -8,8 +8,8 @@ from now.utils import BetterEnum
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.127-secure-playgound-3'
-NOW_PREPROCESSOR_VERSION = '0.0.88-refactor-embed-demo-2'
-NOW_ANNLITE_INDEXER_VERSION = '0.0.6-refactor-embed-demo-2'
+NOW_PREPROCESSOR_VERSION = '0.0.88-refactor-embed-demo-3'
+NOW_ANNLITE_INDEXER_VERSION = '0.0.6-annlite-update-list-endpoint-4'
 
 
 class Modalities(BetterEnum):
