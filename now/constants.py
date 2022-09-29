b21973c12c99e8180c4b1cc4ca22c3df01fea98a
Joschka Braun <joschka.braun@gmail.com>
2022-09-29 16:02:51 +0200

    chore: bump version

--- now/constants.py
+++ now/constants.py
@@ -8,7 +8,7 @@ from now.utils import BetterEnum
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.127-secure-playgound-3'
-NOW_PREPROCESSOR_VERSION = '0.0.89-refactor-embed-demo-2'
+NOW_PREPROCESSOR_VERSION = '0.0.89-refactor-embed-demo-3'
 NOW_ANNLITE_INDEXER_VERSION = '0.0.6-annlite-update-list-endpoint-4'
 
 
