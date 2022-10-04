3ea996901554b751ff36178da8fa78f753931abc
Isabelle Mohr <isabelle.mohr@jina.ai>
2022-10-04 15:25:13 +0200

    test: debug

--- now/constants.py
+++ now/constants.py
@@ -5,10 +5,10 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.127-fix-music-to-music-app-4'
-NOW_PREPROCESSOR_VERSION = '0.0.88-elastic-indexer18'
+NOW_PREPROCESSOR_VERSION = '0.0.88-elastic-indexer19'
 NOW_AUTH_EXECUTOR_VERSION = '0.0.5-auth-exec-4'
 NOW_ANNLITE_INDEXER_VERSION = '0.0.6-annlite-update-list-endpoint-4'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.1-feat-elastic30'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.1-feat-elastic31'
 
 
 class Modalities(BetterEnum):
