53ec2ecb9c5ac56fe7ed9b5598cf680924a4d60a
Florian Hönicke <hoenicke.florian@gmail.com>
2022-10-09 15:19:10 +0200

    test: fix

--- now/constants.py
+++ now/constants.py
@@ -7,7 +7,7 @@ DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.128-add-cname-1'
 NOW_PREPROCESSOR_VERSION = '0.0.89-refactor-base-indexer-4'
-NOW_ANNLITE_INDEXER_VERSION = '0.0.8-feat-sum-ranker-1'
+NOW_ANNLITE_INDEXER_VERSION = '0.0.8-feat-sum-ranker-2'
 
 
 class Modalities(BetterEnum):
