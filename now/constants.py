f8252b4b81ffc0b6c924aa3549ce9bdc0718b44d
Florian Hönicke <hoenicke.florian@gmail.com>
2022-10-11 00:40:53 +0200

    refactor: deactivate annlite ci

--- now/constants.py
+++ now/constants.py
@@ -6,9 +6,9 @@ from now.utils import BetterEnum
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.128-add-cname-1'
 
-NOW_PREPROCESSOR_VERSION = '0.0.91-feat-add-qdrant-indexer-21'
-NOW_ANNLITE_INDEXER_VERSION = '0.0.12-feat-add-qdrant-indexer-21'
-NOW_QDRANT_INDEXER_VERSION = '0.0.1-feat-add-qdrant-indexer-21'
+NOW_PREPROCESSOR_VERSION = '0.0.91-feat-add-qdrant-indexer-22'
+NOW_ANNLITE_INDEXER_VERSION = '0.0.12-feat-add-qdrant-indexer-22'
+NOW_QDRANT_INDEXER_VERSION = '0.0.1-feat-add-qdrant-indexer-22'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.1-feat-elastic-69'
 
 
