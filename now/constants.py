32d756186d9201e48eedff3c563547fe4087c721
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-10-11 17:49:17 +0100

    fix: typo

--- now/constants.py
+++ now/constants.py
@@ -4,11 +4,11 @@ from now.utils import BetterEnum
 # from docarray import __version__ as docarray_version
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.128-feat-auto-complete-3'
-NOW_PREPROCESSOR_VERSION = '0.0.90-feat-auto-complete-3'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.128-feat-auto-complete-4'
+NOW_PREPROCESSOR_VERSION = '0.0.90-feat-auto-complete-4'
 NOW_ANNLITE_INDEXER_VERSION = '0.0.10-feat-elastic-69'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.1-feat-elastic-69'
-NOW_AUTOCOMPLETE_VERSION = '0.0.1-feat-auto-complete-3'
+NOW_AUTOCOMPLETE_VERSION = '0.0.1-feat-auto-complete-4'
 
 
 class Modalities(BetterEnum):
