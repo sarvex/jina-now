cfb1073493ffe4643d77934da6aaa29fdc7e7b8b
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-10-12 15:44:20 +0100

    fix: config for executor

--- now/constants.py
+++ now/constants.py
@@ -4,12 +4,12 @@ from now.utils import BetterEnum
 # from docarray import __version__ as docarray_version
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.128-feat-auto-complete-8'
-NOW_PREPROCESSOR_VERSION = '0.0.91-feat-feat-auto-complete-8'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.128-feat-auto-complete-9'
+NOW_PREPROCESSOR_VERSION = '0.0.91-feat-feat-auto-complete-9'
 NOW_ANNLITE_INDEXER_VERSION = '0.0.12-feat-add-qdrant-indexer-35'
 NOW_QDRANT_INDEXER_VERSION = '0.0.1-feat-add-qdrant-indexer-35'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.2-fix-elastic-1'
-NOW_AUTOCOMPLETE_VERSION = '0.0.1-feat-auto-complete-8'
+NOW_AUTOCOMPLETE_VERSION = '0.0.1-feat-auto-complete-9'
 
 
 class Modalities(BetterEnum):
