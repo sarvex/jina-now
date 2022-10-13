8022a34b6b8d1b425a22797128457d7bcbbb5686
Mohammad Kalim Akram <kalimakram@gmail.com>
2022-10-13 17:32:07 +0200

    refactor: remove now post process (#590)
    * refactor: remove now post process
    
    * fix: remove old file
    
    * fix: bump version
    
    * fix: apply review suggestions
    
    * fix: add test cases

--- now/constants.py
+++ now/constants.py
@@ -8,9 +8,9 @@ from now.utils import BetterEnum
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.128-add-cname-1'
-NOW_PREPROCESSOR_VERSION = '0.0.93-fix-ds-name-1'
+NOW_PREPROCESSOR_VERSION = '0.0.92-refactor-post-process-5'
 NOW_ANNLITE_INDEXER_VERSION = '0.0.12-feat-add-qdrant-indexer-35'
-NOW_QDRANT_INDEXER_VERSION = '0.0.1-feat-add-qdrant-indexer-35'
+NOW_QDRANT_INDEXER_VERSION = '0.0.1-refactor-post-process-5'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.2-fix-elastic-1'
 
 
