a13cfa307ec2ab0e694a27558b4e410dae8add05
Florian Hönicke <hoenicke.florian@gmail.com>
2022-10-16 21:50:37 +0200

    fix: ci

--- now/constants.py
+++ now/constants.py
@@ -7,10 +7,10 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.129-refactor-executors-17'
-NOW_PREPROCESSOR_VERSION = '0.0.93-refactor-executors-17'
-NOW_QDRANT_INDEXER_VERSION = '0.0.2-refactor-executors-17'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.3-refactor-executors-17'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.129-refactor-executors-18'
+NOW_PREPROCESSOR_VERSION = '0.0.93-refactor-executors-18'
+NOW_QDRANT_INDEXER_VERSION = '0.0.2-refactor-executors-18'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.3-refactor-executors-18'
 
 
 class Modalities(BetterEnum):
