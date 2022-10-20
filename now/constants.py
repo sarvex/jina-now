6ce0be4837292d39a7b87e068bf92f60001b9e27
Isabelle Mohr <isabelle.mohr@jina.ai>
2022-10-20 14:21:56 +0200

    style: black

--- now/constants.py
+++ now/constants.py
@@ -7,8 +7,8 @@ from now.utils import BetterEnum
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.129-test-multimodal-demo-15'
-NOW_PREPROCESSOR_VERSION = '0.0.93-test-multimodal-demo-15'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.129-test-multimodal-demo-16'
+NOW_PREPROCESSOR_VERSION = '0.0.93-test-multimodal-demo-16'
 NOW_QDRANT_INDEXER_VERSION = '0.0.2-refactor-executors-21'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.3-refactor-executors-21'
 
