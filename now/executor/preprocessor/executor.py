e6625628ba136d95d931d9466d3a88d2ecd5b824
jupyterjazz <saba.sturua@jina.ai>
2022-10-25 14:59:50 +0200

    refactor: go back to previous code

--- now/executor/preprocessor/executor.py
+++ now/executor/preprocessor/executor.py
@@ -39,10 +39,8 @@ class NOWPreprocessor(Executor):
             with open(self.user_input_path, 'r') as fp:
                 user_input = json.load(fp)
             self._set_user_input({'user_input': {**user_input}})
-        elif 'user_input' in kwargs:
-            self.user_input = UserInput(**kwargs['user_input'])
         else:
-            raise Exception('user input is not provided.')
+            self.user_input = None
 
     def _set_user_input(self, parameters: Dict):
         """Sets user_input attribute and deletes used attributes from dictionary"""
