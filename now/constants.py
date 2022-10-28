479cf198b8f98ce1d8d05f408260445ecfb20951
Aziz Belaweid <40893766+azayz@users.noreply.github.com>
2022-10-28 15:14:16 +0100

    refactor: autocomplete (#624)
    * Revert "Refactor revert autocomplete issue (#622)"
    
    This reverts commit 4fc83efd2f1f10f8b4ddd29af53e60c5ee685686.
    
    * refactor: remove autocomplete from playground

--- now/constants.py
+++ now/constants.py
@@ -5,12 +5,13 @@ from now.utils import BetterEnum
 # TODO: Uncomment the DEMO_DATASET_DOCARRAY_VERSION when the DocArray datasets on GCloud has been changed
 # from docarray import __version__ as docarray_version
 # DEMO_DATASET_DOCARRAY_VERSION = docarray_version
-DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.129-revert-refactor-executors-2'
-NOW_PREPROCESSOR_VERSION = '0.0.94-revert-refactor-executors-2'
+DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.129-feat-auto-complete-51'
+NOW_PREPROCESSOR_VERSION = '0.0.93-feat-auto-complete-51'
 NOW_QDRANT_INDEXER_VERSION = '0.0.2-allow-domain-6'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.3-refactor-executors-21'
+NOW_AUTOCOMPLETE_VERSION = '0.0.1-feat-auto-complete-52'
 
 
 class Modalities(BetterEnum):
