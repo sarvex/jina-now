4b5e12d9c51548502599809312ada3e9da419e09
Isabelle Mohr <isabelle.mohr@jina.ai>
2022-11-01 14:24:39 +0100

    test: fix asserts

--- tests/unit/data_loading/test_data_loading.py
+++ tests/unit/data_loading/test_data_loading.py
@@ -133,8 +133,7 @@ def test_es_online_shop_ds(da: DocumentArray):
 
     for doc in loaded_da:
         assert doc.chunks
-        assert doc.chunks[0].text
-        assert doc.chunks[1].uri
+        assert (doc.chunks[0].text and doc.chunks[1].uri) or (doc.chunks[0].uri and doc.chunks[1].text)
 
 
 @pytest.fixture
