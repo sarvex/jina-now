8df747b57d30090694072b8e626202263667fbdb
jupyterjazz <saba.sturua@jina.ai>
2022-11-03 17:05:34 +0100

    chore: add prints

--- now/executor/abstract/base_indexer/base_indexer.py
+++ now/executor/abstract/base_indexer/base_indexer.py
@@ -146,11 +146,12 @@ class NOWBaseIndexer(Executor):
         search_filter = parameters.get('filter', {})
         search_filter = self.convert_filter_syntax(search_filter)
         traversal_paths = parameters.get('traversal_paths', self.traversal_paths)
-        new_docs = docs[traversal_paths][
-            :1
-        ]  # only search on the first document for now
+
+        docs = docs[traversal_paths][:1]  # only search on the first document for now
+        for d in docs:
+            if d.embedding is None:
+                raise Exception(f'{d} search endpoint gets no embeddings')
         # self.check_docs(new_docs, docs)
-        docs = new_docs
         if traversal_paths == '@c,c':
             retrieval_limit = limit * 3
         else:
