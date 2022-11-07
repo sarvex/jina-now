6709b440b1f0ed4605909a034de0e80925d17104
Isabelle Mohr <isabelle.mohr@jina.ai>
2022-11-07 13:34:43 +0100

    refactor: remove prints from ElasticIndexer

--- now/constants.py
+++ now/constants.py
@@ -6,10 +6,10 @@ from now.utils import BetterEnum
 # from docarray import __version__ as docarray_version
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.130-test-multimodal-17'
-NOW_PREPROCESSOR_VERSION = '0.0.93-test-multimodal-17'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.130-test-multimodal-18'
+NOW_PREPROCESSOR_VERSION = '0.0.93-test-multimodal-18'
 NOW_QDRANT_INDEXER_VERSION = '0.0.2-debug-issue-1'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.3-test-multimodal-17'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.3-test-multimodal-18'
 NOW_AUTOCOMPLETE_VERSION = '0.0.1-feat-auto-complete-52'
 
 
--- now/executor/indexer/elastic/elastic_indexer.py
+++ now/executor/indexer/elastic/elastic_indexer.py
@@ -116,8 +116,6 @@ class ElasticIndexer(Executor):
                 docs = docs_matrix[0]
         if not parameters:
             parameters = {}
-        docs.summary()
-        docs[0].summary()
         traversal_paths = parameters.get('traversal_paths', self.traversal_paths)
         es_docs = self._transform_docs_to_es(docs, traversal_paths)
         try:
@@ -170,9 +168,6 @@ class ElasticIndexer(Executor):
                 )
             else:
                 docs = docs_matrix[0]
-        docs.summary()
-        docs[0].summary()
-        print(parameters)
         traversal_paths = parameters.get('traversal_paths', self.traversal_paths)
         search_filter = parameters.get('filter', None)
         limit = parameters.get('limit', 20)
