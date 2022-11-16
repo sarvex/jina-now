9bea036d7c92ba4865510c222b9df36dc3fa6462
Isabelle Mohr <isabelle.mohr@jina.ai>
2022-11-16 13:27:54 +0100

    refactor: remove prints from test

--- tests/unit/data_loading/test_data_loading.py
+++ tests/unit/data_loading/test_data_loading.py
@@ -52,7 +52,6 @@ def local_da(da: DocumentArray, tmpdir: str) -> Tuple[str, DocumentArray]:
 def is_da_text_equal(da_a: DocumentArray, da_b: DocumentArray):
     for a, b in zip(da_a, da_b):
         if a.text != b.text:
-            print(a.text, b.text)
             return False
     return True
 
