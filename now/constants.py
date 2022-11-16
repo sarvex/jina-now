4869d13629348665ef5c7f72e2bdd19ae348d30c
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-11-16 16:08:35 +0100

    fix: tags

--- now/constants.py
+++ now/constants.py
@@ -6,8 +6,8 @@ from now.utils import BetterEnum
 # from docarray import __version__ as docarray_version
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.130-feat-migrate-jcloud-1'
-NOW_PREPROCESSOR_VERSION = '0.0.97-feat-migrate-jcloud-1'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.130-feat-migrate-jcloud-2'
+NOW_PREPROCESSOR_VERSION = '0.0.97-feat-migrate-jcloud-2'
 NOW_QDRANT_INDEXER_VERSION = '0.0.5-refactor-wrap-auth-executor-2'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.4-refactor-wrap-auth-executor-2'
 NOW_AUTOCOMPLETE_VERSION = '0.0.2-refactor-wrap-auth-executor-2'
