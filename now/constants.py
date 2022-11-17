721cf951ca1dd12cb28a7833530ba47ec82806d0
Joschka Braun <joschka.braun@gmail.com>
2022-11-17 14:37:25 +0100

    fix: fix test requirements

--- now/constants.py
+++ now/constants.py
@@ -7,8 +7,8 @@ from now.utils import BetterEnum
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.131-refactor-breakpoints-in-executors-11'
-NOW_PREPROCESSOR_VERSION = '0.0.99-ocr-in-preprocessor-2'
-NOW_QDRANT_INDEXER_VERSION = '0.0.6-ocr-in-preprocessor-2'
+NOW_PREPROCESSOR_VERSION = '0.0.99-ocr-in-preprocessor-3'
+NOW_QDRANT_INDEXER_VERSION = '0.0.6-ocr-in-preprocessor-3'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.5-refactor-force-push-1'
 NOW_AUTOCOMPLETE_VERSION = '0.0.4-refactor-force-push-1'
 
