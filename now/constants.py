ec07b85dc7d125258276387241ab6730b9dde856
Florian Hönicke <hoenicke.florian@gmail.com>
2022-11-23 00:00:18 +0100

    feat: deactivate telemetry on ci

--- now/constants.py
+++ now/constants.py
@@ -6,8 +6,8 @@ from now.utils import BetterEnum
 # from docarray import __version__ as docarray_version
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.132-feat-migrate-jcloud-10'
-NOW_PREPROCESSOR_VERSION = '0.0.102-feat-deactivate-telemetry-on-ci-6'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.132-bff-cors-2'
+NOW_PREPROCESSOR_VERSION = '0.0.102-feat-deactivate-telemetry-on-ci-7'
 NOW_QDRANT_INDEXER_VERSION = '0.0.7-feat-new-data-format-127'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.6-feat-new-data-format-127'
 NOW_AUTOCOMPLETE_VERSION = '0.0.4-feat-new-data-format-127'
