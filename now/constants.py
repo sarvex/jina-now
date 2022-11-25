b990a34f4ec6bda99377c711e2fa19efbda96c50
Florian Hönicke <hoenicke.florian@gmail.com>
2022-11-26 00:15:43 +0100

    refactor: bump version

--- now/constants.py
+++ now/constants.py
@@ -7,7 +7,7 @@ from now.utils import BetterEnum
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.133-refactor-remove-apps-4'
-NOW_PREPROCESSOR_VERSION = '0.0.102-fix-local-deployment-1'
+NOW_PREPROCESSOR_VERSION = '0.0.103-fix-text-to-video-1'
 NOW_QDRANT_INDEXER_VERSION = '0.0.7-feat-new-data-format-127'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.6-feat-new-data-format-127'
 NOW_AUTOCOMPLETE_VERSION = '0.0.4-refactor-remove-app-4'
