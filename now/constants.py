ddde4085d94ca1fcb6d703afed7e58b5101acca9
Florian Hönicke <hoenicke.florian@gmail.com>
2022-11-27 21:59:15 +0100

    fix: version

--- now/constants.py
+++ now/constants.py
@@ -6,11 +6,11 @@ from now.utils import BetterEnum
 # from docarray import __version__ as docarray_version
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.134-refactor-simplify-preprocessing-27'
-NOW_PREPROCESSOR_VERSION = '0.0.104-refactor-simplify-preprocessing-27'
-NOW_QDRANT_INDEXER_VERSION = '0.0.8-refactor-simplify-preprocessing-27'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.7-refactor-simplify-preprocessing-27'
-NOW_AUTOCOMPLETE_VERSION = '0.0.4-refactor-simplify-preprocessing-27'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.134-refactor-simplify-preprocessing-28'
+NOW_PREPROCESSOR_VERSION = '0.0.104-refactor-simplify-preprocessing-28'
+NOW_QDRANT_INDEXER_VERSION = '0.0.8-refactor-simplify-preprocessing-28'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.7-refactor-simplify-preprocessing-28'
+NOW_AUTOCOMPLETE_VERSION = '0.0.4-refactor-simplify-preprocessing-28'
 
 
 class Modalities(BetterEnum):
