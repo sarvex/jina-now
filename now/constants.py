e3b0bd9ad3eb563d74b03e6c2613684734229a46
Mohammad Kalim Akram <kalim.akram@jina.ai>
2022-11-30 10:20:24 +0100

    fix: version

--- now/constants.py
+++ now/constants.py
@@ -6,11 +6,11 @@ from now.utils import BetterEnum
 # from docarray import __version__ as docarray_version
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.134-top_k_fix-4'
-NOW_PREPROCESSOR_VERSION = '0.0.103-fix-text-to-video-5'
-NOW_QDRANT_INDEXER_VERSION = '0.0.7-feat-new-data-format-127'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.6-feat-new-data-format-127'
-NOW_AUTOCOMPLETE_VERSION = '0.0.4-refactor-remove-app-4'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.134-top_k_fix-5'
+NOW_PREPROCESSOR_VERSION = '0.0.104-refactor-simplify-preprocessing-45'
+NOW_QDRANT_INDEXER_VERSION = '0.0.8-refactor-simplify-preprocessing-45'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.7-refactor-simplify-preprocessing-45'
+NOW_AUTOCOMPLETE_VERSION = '0.0.4-refactor-simplify-preprocessing-45'
 
 
 class Modalities(BetterEnum):
