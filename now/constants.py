10441d0a3151d9400ce9c63e22748efe58aa7eb1
Florian Hönicke <hoenicke.florian@gmail.com>
2022-12-05 00:25:33 +0100

    fix: wolf, loading, duplicates, test

--- now/constants.py
+++ now/constants.py
@@ -7,9 +7,9 @@ from now.utils import BetterEnum
 
 DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.134-top_k_fix-8'
-NOW_PREPROCESSOR_VERSION = '0.0.104-refactor-simplify-preprocessing-45'
-NOW_QDRANT_INDEXER_VERSION = '0.0.8-refactor-simplify-preprocessing-45'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.7-refactor-simplify-preprocessing-45'
+NOW_PREPROCESSOR_VERSION = '0.0.105-fix-different-search-term-after-curation-1'
+NOW_QDRANT_INDEXER_VERSION = '0.0.8-fix-different-search-term-after-curation-1'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.7-fix-different-search-term-after-curation-1'
 NOW_AUTOCOMPLETE_VERSION = '0.0.4-refactor-simplify-preprocessing-45'
 
 
@@ -63,7 +63,7 @@ CLIP_USES = {
     'local': ('CLIPOnnxEncoder/latest', 'ViT-B-32::openai', ModelDimensions.CLIP),
     'remote': ('CLIPOnnxEncoder/latest-gpu', 'ViT-B-32::openai', ModelDimensions.CLIP),
 }
-EXTERNAL_CLIP_HOST = 'encoderclip-game-gar-c70a0d0c79.wolf.jina.ai'
+EXTERNAL_CLIP_HOST = 'encoderclip-climbing-lobster-885354f905.wolf.jina.ai'
 
 DEFAULT_FLOW_NAME = 'nowapi'
 PREFETCH_NR = 10
