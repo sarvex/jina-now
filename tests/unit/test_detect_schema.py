7490718f2db219944a2a8214093eb0b59ca3bafc
Aziz Belaweid <azizbellaweid@hotmail.com>
2022-12-05 09:42:50 +0100

    fix: use fixture

--- tests/unit/test_detect_schema.py
+++ tests/unit/test_detect_schema.py
@@ -1,7 +1,19 @@
 import os
 
+import pytest
+
 from now.common.detect_schema import _get_schema_local_folder, _get_schema_s3_bucket
 from now.now_dataclasses import UserInput
+
+
+@pytest.fixture
+def get_aws_info():
+    dataset_path = os.environ.get('S3_SCHEMA_FOLDER_PATH')
+    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    region = 'eu-west-1'
+
+    return dataset_path, aws_access_key_id, aws_secret_access_key, region
 
 
 def test_get_schema_local_folder(gif_resource_path):
@@ -23,12 +35,14 @@ def test_get_schema_local_folder_all_files(image_resource_path):
     assert len(user_input.field_names) == 0
 
 
-def test_get_schema_s3_bucket():
+def test_get_schema_s3_bucket(get_aws_info):
     user_input = UserInput()
-    user_input.dataset_path = os.environ.get('S3_SCHEMA_FOLDER_PATH')
-    user_input.aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-    user_input.aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    user_input.aws_region_name = 'eu-west-1'
+    (
+        user_input.dataset_path,
+        user_input.aws_access_key_id,
+        user_input.aws_secret_access_key,
+        user_input.aws_region_name,
+    ) = get_aws_info()
 
     _get_schema_s3_bucket(user_input)
 
@@ -45,9 +59,13 @@ def test_get_schema_s3_bucket():
 
 def test_get_schema_s3_bucket_all_files():
     user_input = UserInput()
-    user_input.dataset_path = os.environ.get('S3_SCHEMA_FOLDER_PATH') + 'folder1/'
-    user_input.aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-    user_input.aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    (
+        user_input.dataset_path,
+        user_input.aws_access_key_id,
+        user_input.aws_secret_access_key,
+        user_input.aws_region_name,
+    ) = get_aws_info()
+
     _get_schema_s3_bucket(user_input)
 
     assert len(user_input.field_names) == 0
