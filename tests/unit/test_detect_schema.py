6dbac6d1f5fff9c1923c29bceb1a30643a846f3d
azayz <azizbellaweid@hotmail.com>
2022-12-08 17:07:14 +0100

    fix: apply comments

--- tests/unit/test_detect_schema.py
+++ tests/unit/test_detect_schema.py
@@ -22,26 +22,40 @@ def get_aws_info():
     return dataset_path, aws_access_key_id, aws_secret_access_key, region
 
 
-def test_set_field_names_from_local_folder(gif_resource_path):
+@pytest.mark.parametrize(
+    'dataset_path, field_names',
+    [
+        ('gif_resource_path', {'file.txt', 'file.gif', 'a1', 'a2'}),
+        ('image_resource_path', set()),
+    ],
+)
+def test_set_fields_names_from_local_folder(dataset_path, field_names, request):
     user_input = UserInput()
-    user_input.dataset_path = gif_resource_path
+    user_input.dataset_path = request.getfixturevalue(dataset_path)
 
     set_field_names_from_local_folder(user_input)
 
-    assert len(user_input.field_names) == 4
-    assert set(user_input.field_names) == {'file.txt', 'file.gif', 'a1', 'a2'}
+    assert set(user_input.field_names) == field_names
 
 
-def test_set_field_names_from_local_folder_all_files(image_resource_path):
-    user_input = UserInput()
-    user_input.dataset_path = image_resource_path
-
-    set_field_names_from_local_folder(user_input)
-
-    assert len(user_input.field_names) == 0
-
-
-def test_set_field_names_from_s3_bucket(get_aws_info):
+@pytest.mark.parametrize(
+    'dataset_path, field_names',
+    [
+        (
+            '',
+            {
+                'image.png',
+                'test.txt',
+                'tags',
+                'id',
+                'link',
+                'title',
+            },
+        ),
+        ('folder1/', set()),
+    ],
+)
+def test_set_field_names_from_s3_bucket(dataset_path, field_names, get_aws_info):
     user_input = UserInput()
     (
         user_input.dataset_path,
@@ -49,32 +63,11 @@ def test_set_field_names_from_s3_bucket(get_aws_info):
         user_input.aws_secret_access_key,
         user_input.aws_region_name,
     ) = get_aws_info
+    user_input.dataset_path = user_input.dataset_path + dataset_path
 
     set_field_names_from_s3_bucket(user_input)
 
-    assert len(user_input.field_names) == 6
-    assert set(user_input.field_names) == {
-        'image.png',
-        'test.txt',
-        'tags',
-        'id',
-        'link',
-        'title',
-    }
-
-
-def test_set_field_names_from_s3_bucket_all_files(get_aws_info):
-    user_input = UserInput()
-    (
-        user_input.dataset_path,
-        user_input.aws_access_key_id,
-        user_input.aws_secret_access_key,
-        user_input.aws_region_name,
-    ) = get_aws_info
-    user_input.dataset_path = user_input.dataset_path + 'folder1/'
-    set_field_names_from_s3_bucket(user_input)
-
-    assert len(user_input.field_names) == 0
+    assert set(user_input.field_names) == field_names
 
 
 def test_set_field_names_from_docarray():
