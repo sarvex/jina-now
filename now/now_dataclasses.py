4ccd2cc4966e2e337b7d92ba9b820ab9b01c9e29
azayz <azizbellaweid@hotmail.com>
2022-12-09 18:07:51 +0100

    fix: apply comment

--- now/now_dataclasses.py
+++ now/now_dataclasses.py
@@ -103,8 +103,8 @@ class UserInput(BaseModel):
     aws_region_name: Optional[str] = None
 
     # Fields
-    search_fields = Optional[List] = []
-    filter_fields = Optional[List] = []
+    search_fields: Optional[List] = []
+    filter_fields: Optional[List] = []
     search_fields_modalities: Optional[Dict] = {}
     filter_fields_modalities: Optional[List] = []
 
