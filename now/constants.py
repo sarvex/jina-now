d073f02029f7a9e0436902d9131a7f5794f949be
Florian Hönicke <hoenicke.florian@gmail.com>
2022-12-11 22:13:46 +0100

    fix: bump version

--- now/constants.py
+++ now/constants.py
@@ -9,10 +9,10 @@ DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 # ----------------------------------
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.135-test-reactivate'
 # ----------------------------------
-NOW_PREPROCESSOR_VERSION = '0.0.109-refactor-remove-redundancy-on-ci-10'
-NOW_QDRANT_INDEXER_VERSION = '0.0.10-refactor-remove-redundancy-on-ci-10'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.9-refactor-remove-redundancy-on-ci-10'
-NOW_AUTOCOMPLETE_VERSION = '0.0.5-refactor-remove-redundancy-on-ci-10'
+NOW_PREPROCESSOR_VERSION = '0.0.109-refactor-remove-redundancy-on-ci-11'
+NOW_QDRANT_INDEXER_VERSION = '0.0.10-refactor-remove-redundancy-on-ci-11'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.9-refactor-remove-redundancy-on-ci-11'
+NOW_AUTOCOMPLETE_VERSION = '0.0.5-refactor-remove-redundancy-on-ci-11'
 
 
 class Modalities(BetterEnum):
