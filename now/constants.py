cc50e8411aaa17b9a2b49135af08bd68637cf88b
Florian Hönicke <hoenicke.florian@gmail.com>
2022-12-12 21:24:13 +0100

    refactor: optimize build time

--- now/constants.py
+++ now/constants.py
@@ -9,10 +9,10 @@ DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 # ----------------------------------
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.135-test-reactivate'
 # ----------------------------------
-NOW_PREPROCESSOR_VERSION = '0.0.109-refactor-remove-redundancy-on-ci-17'
-NOW_QDRANT_INDEXER_VERSION = '0.0.10-refactor-remove-redundancy-on-ci-17'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.9-refactor-remove-redundancy-on-ci-17'
-NOW_AUTOCOMPLETE_VERSION = '0.0.5-refactor-remove-redundancy-on-ci-17'
+NOW_PREPROCESSOR_VERSION = '0.0.109-refactor-remove-redundancy-on-ci-18'
+NOW_QDRANT_INDEXER_VERSION = '0.0.10-refactor-remove-redundancy-on-ci-18'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.9-refactor-remove-redundancy-on-ci-18'
+NOW_AUTOCOMPLETE_VERSION = '0.0.5-refactor-remove-redundancy-on-ci-18'
 
 
 class Modalities(BetterEnum):
