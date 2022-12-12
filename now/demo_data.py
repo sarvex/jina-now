3323dcc377bb3293311e1e45c610f7e1bcdd97d8
Florian Hönicke <hoenicke.florian@gmail.com>
2022-12-12 23:29:36 +0100

    fix: import

--- now/demo_data.py
+++ now/demo_data.py
@@ -4,7 +4,7 @@ from docarray import DocumentArray
 from pydantic import BaseModel
 
 from now.constants import Modalities
-from now.data_loading.utils import fetch_da_from_url, get_dataset_url
+from now.data_loading.data_loading import fetch_da_from_url, get_dataset_url
 from now.utils import BetterEnum
 
 
