7dcb5f5357ec1a1445db5f827e094ac42f59c94b
Isabelle Mohr <isabelle.mohr@jina.ai>
2022-12-15 11:19:09 +0100

    fix: requirements

--- now/constants.py
+++ now/constants.py
@@ -9,10 +9,10 @@ DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 # ----------------------------------
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.135-test-reactivate'
 # ----------------------------------
-NOW_PREPROCESSOR_VERSION = '0.0.113-elastic-multimodal-1'
-NOW_QDRANT_INDEXER_VERSION = '0.0.11-elastic-multimodal-1'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.10-elastic-multimodal-1'
-NOW_AUTOCOMPLETE_VERSION = '0.0.6-elastic-multimodal-1'
+NOW_PREPROCESSOR_VERSION = '0.0.113-elastic-multimodal-2'
+NOW_QDRANT_INDEXER_VERSION = '0.0.11-elastic-multimodal-2'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.10-elastic-multimodal-2'
+NOW_AUTOCOMPLETE_VERSION = '0.0.6-elastic-multimodal-2'
 
 
 class Modalities(BetterEnum):
