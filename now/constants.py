fb55e6b0cc8b154815a5bb551aa1a4c2d54f992c
Mohammad Kalim Akram <kalim.akram@jina.ai>
2022-12-16 09:52:19 +0100

    fix: broken test

--- now/constants.py
+++ now/constants.py
@@ -3,10 +3,10 @@ from __future__ import annotations, print_function, unicode_literals
 from now.utils import BetterEnum
 
 # ----------------------------------
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.136-demo-data-migration-11'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.136-demo-data-migration-12'
 # ----------------------------------
 NOW_PREPROCESSOR_VERSION = '0.0.114-fix-disable-telemetry'
-NOW_QDRANT_INDEXER_VERSION = '0.0.12-demo-data-migration-11'
+NOW_QDRANT_INDEXER_VERSION = '0.0.12-demo-data-migration-12'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.10-enable-local-testing-4'
 NOW_AUTOCOMPLETE_VERSION = '0.0.6-enable-local-testing-4'
 
