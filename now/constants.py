00d059bc6bd27b3377a4055266b4e47d302cbaa8
Isabelle Mohr <retrospect@protonmail.com>
2022-12-19 13:17:10 +0100

    feat: n-to-n ElasticIndexer multi modal doc (#735)
    * feat: changed init. index not yet working
    
    * feat: fix indexing (#691)
    
    * feat: allow multimdodal doc indexing with elastic
    
    * test: add es service running fixture
    
    * refactor: remove typo and comment unused functions
    
    * refactor: remove commented code and cleanup prints
    
    * test: add tests for docs transforms in indexer
    
    * feat: enable search
    
    * feat: update list endpoint of elasticindexer (#698)
    
    * refactor: update list endpoint with limit and offset
    
    * test: add test for list endpoint, refactor es_inputs as fixture
    
    * feat: add score breakdown for matches using elasticindexer (#708)
    
    * feat: add breakdown of semantic score to matches
    
    * test: add assert about score breakdown
    
    * refactor: address review comments, enable bm25 in score breakdown
    
    * refactor: return raw + normalized bm25 score, add delete by ID
    
    * test: assert about matches bm25 scores
    
    * test: add test for delete by id and score calculation
    
    * feat: allow custom bm25 query, serialize doc (#722)
    
    * feat: allow custom bm25 query, requires custom es_mapping
    
    * feat: serialize whole doc at indexing, return whole at searching
    
    * refactor: move conversion of da to es_converter (#733)
    
    * refactor: move conversion of da to es_converter
    
    * test: fix test es_maping
    
    * fix: remove .env file
    
    * refactor: add default values to baseindexer
    
    * refactor: elasticindexer extends baseindexer
    
    * refactor: cleanup and remove prints
    
    * refactor: move query building logic, add automatic semantic scoring
    
    * refactor: make semantic_scores optional for indexer init
    
    * refactor: revert changes, no more skip connection from preprocessor
    
    * refactor: add tests for query building
    
    * refactor: move executor tests
    
    * test: add executor tests for nowelasticindexer
    
    * test: enable test for removing blob and tensor from matches
    
    * feat: add /tags endpoint to elasticindexer
    
    * test: enable tags test for elasticindexer
    
    * test: add tags to conftest for elasticindexer executor tests
    
    * chore: update requirements
    
    * feat: enable curation endpoint on elasticindexer
    
    * fix: update constants
    
    * fix: requirements
    
    * fix: update constants
    
    * fix: update versions
    
    * fix: update constants
    
    * fix: update constants
    
    * chore: update qdrant image
    
    * fix: bug in tests
    
    * fix: increase timeout for executor tests
    
    * fix: increase timeout for executor tests
    
    * refactor: address review comments
    
    * refactor: address review comments
    
    * chore: update constants
    
    * chore: update constants
    
    * refactor: update docstrings for review comment
    
    Co-authored-by: Maximilian Werk <maximilian.werk@jina.ai>

--- now/constants.py
+++ now/constants.py
@@ -9,10 +9,10 @@ DEMO_DATASET_DOCARRAY_VERSION = '0.13.17'
 # ----------------------------------
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.135-refactor-output_modality-29'
 # ----------------------------------
-NOW_PREPROCESSOR_VERSION = '0.0.114-refactor-output_modality-29'
-NOW_QDRANT_INDEXER_VERSION = '0.0.11-enable-local-testing-4'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.10-enable-local-testing-4'
-NOW_AUTOCOMPLETE_VERSION = '0.0.6-enable-local-testing-4'
+NOW_PREPROCESSOR_VERSION = '0.0.114-elastic-multimodal-15x'
+NOW_QDRANT_INDEXER_VERSION = '0.0.11-elastic-multimodal-15x'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.10-elastic-multimodal-15x'
+NOW_AUTOCOMPLETE_VERSION = '0.0.6-elastic-multimodal-15x'
 
 
 class Modalities(BetterEnum):
