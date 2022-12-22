fc9509e739c2138425b7cf08064afb215edd0d14
azayz <azizbellaweid@hotmail.com>
2022-12-22 11:47:16 +0100

    test: fix test

--- now/run_backend.py
+++ now/run_backend.py
@@ -153,6 +153,8 @@ def call_flow(
 
     # Pop app_instance from parameters to be passed to the flow
     parameters['user_input'].pop('app_instance', None)
+    parameters['user_input'].pop('search_fields_modalities', None)
+    parameters['user_input'].pop('filter_fields_modalities', None)
     task_config = parameters['user_input'].pop('task_config', None)
     if task_config:
         parameters['user_input']['indexer_scope'] = task_config.indexer_scope
