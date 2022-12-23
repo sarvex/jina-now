2fdd1933777046c18fa85fec830b2c66c73bd263
azayz <azizbellaweid@hotmail.com>
2022-12-23 17:00:29 +0100

    fix: test

--- now/run_backend.py
+++ now/run_backend.py
@@ -228,7 +228,6 @@ def create_dataclass(user_input: UserInput):
     file_mapping_to_dataclass_fields = create_dataclass_fields_file_mappings(
         user_input.search_fields + user_input.filter_fields,
         all_modalities,
-        user_input.dataset_type,
     )
     user_input.files_to_dataclass_fields = file_mapping_to_dataclass_fields
     (all_annotations, all_class_attributes,) = create_annotations_and_class_attributes(
