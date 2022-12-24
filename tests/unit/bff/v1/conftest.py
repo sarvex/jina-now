8c90e52f9e5e5ce0d77411b9def3b5953729ce52
Florian Hönicke <hoenicke.florian@gmail.com>
2022-12-24 08:41:49 +0100

    refactor: conftest (#817)

--- tests/unit/bff/v1/conftest.py
+++ tests/unit/bff/v1/conftest.py
@@ -1,28 +1,5 @@
-import base64
-import os
-
 import pytest
 from docarray import Document, DocumentArray
-
-
-@pytest.fixture
-def base64_image_string(resources_folder_path: str) -> str:
-    with open(
-        os.path.join(resources_folder_path, 'image', '5109112832.jpg'), 'rb'
-    ) as f:
-        binary = f.read()
-        img_string = base64.b64encode(binary).decode('utf-8')
-    return img_string
-
-
-@pytest.fixture
-def base64_image_string(resources_folder_path: str) -> str:
-    with open(
-        os.path.join(resources_folder_path, 'image', '5109112832.jpg'), 'rb'
-    ) as f:
-        binary = f.read()
-        img_string = base64.b64encode(binary).decode('utf-8')
-    return img_string
 
 
 @pytest.fixture
