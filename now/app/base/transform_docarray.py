9535476de7255933037de4f8802aaffcb2f748c3
Florian Hönicke <hoenicke.florian@gmail.com>
2022-12-26 16:56:35 +0100

    fix: braces

--- now/app/base/transform_docarray.py
+++ now/app/base/transform_docarray.py
@@ -113,6 +113,6 @@ def transform_docarray(
     :param documents: Data to be transformed.
     :param search_fields: Field names for neural search. Only required if multimodal data is given.
     """
-    if not documents and documents[0].chunks:
+    if not (documents and documents[0].chunks):
         documents = transform_uni_modal_data(documents=documents)
     return documents
