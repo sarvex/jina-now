748b68dacc149477ff2b889538b03f2930c4b6ba
azayz <azizbellaweid@hotmail.com>
2022-12-26 12:52:11 +0100

    fix: test

--- now/constants.py
+++ now/constants.py
@@ -8,11 +8,10 @@ from now.utils import BetterEnum
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.138-fix-search-options-order1'
 # ----------------------------------
 
-NOW_PREPROCESSOR_VERSION = '0.0.115-feat-dataclass-dataloading-12'
+NOW_PREPROCESSOR_VERSION = '0.0.115-feat-dataclass-dataloading-13'
 NOW_QDRANT_INDEXER_VERSION = '0.0.138-refactor-remove-finetuning-6'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.138-refactor-remove-finetuning-6'
 NOW_AUTOCOMPLETE_VERSION = '0.0.7-refactor-remove-finetuning-6'
-
 
 
 class Modalities(BetterEnum):
