de4e70ed6c81a300176b90bc5bdf0e5ca86de647
azayz <azizbellaweid@hotmail.com>
2022-12-27 16:59:47 +0100

    fix: test

--- now/constants.py
+++ now/constants.py
@@ -5,9 +5,9 @@ from docarray.typing import Image, Text, Video
 from now.utils import BetterEnum
 
 # ----------------------------------
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.138-fix-search-options-order1'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.139-fix-dataclass'
 # ----------------------------------
-NOW_PREPROCESSOR_VERSION = '0.0.117-test-local-end-to-end-23'
+NOW_PREPROCESSOR_VERSION = '0.0.117-test-local-end-to-end-24'
 NOW_QDRANT_INDEXER_VERSION = '0.0.138-refactor-remove-finetuning-6'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.139-test-local-end-to-end-12'
 NOW_AUTOCOMPLETE_VERSION = '0.0.7-refactor-remove-finetuning-6'
