8faa79818e2c57cb2bde9e58c3f18ada046717b8
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2022-12-28 10:51:30 +0100

    fix: removed extra files

--- test-executor.py
+++
@@ -1,8 +0,0 @@
-from jina import Flow
-
-with Flow().add(
-    uses='jinahub+docker://CLIPOnnxEncoder/0.8.1',
-    uses_with={'name': 'ViT-B-32::openai'},
-    env={'JINA_LOG_LEVEL': 'DEBUG'},
-) as f:
-    pass
