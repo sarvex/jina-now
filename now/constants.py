87e54dd2cf43b748c2769789428719ae32039031
Florian Hönicke <hoenicke.florian@gmail.com>
2023-01-02 01:55:15 +0100

    fix: constants

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.139-refactor-rename-fields-19'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.140-feat-integrate-elastic-16'
 NOW_PREPROCESSOR_VERSION = '0.0.117-refactor-rename-fields-19'
 NOW_QDRANT_INDEXER_VERSION = '0.0.138-refactor-remove-finetuning-6'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.140-feat-integrate-elastic-15'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.140-feat-integrate-elastic-16'
 NOW_AUTOCOMPLETE_VERSION = '0.0.7-create-mm-docs-in-bff-18'
 
 
