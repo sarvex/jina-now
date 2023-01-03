77da5073e957267b732cbb878d563220ffe622bc
Joschka Braun <joschka.braun@gmail.com>
2023-01-03 11:41:53 +0100

    fix: sbert parameters

--- now/app/search_app/app.py
+++ now/app/search_app/app.py
@@ -124,7 +124,7 @@ class SearchApp(JinaNOWApp):
         return {
             'name': 'sbert_encoder',
             'uses': f'{EXECUTOR_PREFIX}TransformerSentenceEncoder',
-            'uses_with': {'name': 'msmarco-distilbert-base-v3'},
+            'uses_with': {'model_name': 'msmarco-distilbert-base-v3'},
             'env': {'JINA_LOG_LEVEL': 'DEBUG'},
             'needs': 'preprocessor',
         }, 768
