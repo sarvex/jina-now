72a362fbffb8bee4217df25d17bad5de0cf64532
Florian Hönicke <hoenicke.florian@gmail.com>
2023-01-03 23:55:49 +0100

    refactor: remove logs

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.139-feat-integrate-elastic-32'
-NOW_PREPROCESSOR_VERSION = '0.0.118-feat-integrate-elastic-32'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.140-feat-integrate-elastic-32'
-NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-integrate-elastic-32'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.139-feat-integrate-elastic-33'
+NOW_PREPROCESSOR_VERSION = '0.0.118-feat-integrate-elastic-33'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.140-feat-integrate-elastic-33'
+NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-integrate-elastic-33'
 
 
 class Modalities(BetterEnum):
