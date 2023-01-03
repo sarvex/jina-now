bc2e71ac0b0c408448447f6d176e4f0e95166cfb
Joschka Braun <joschka.braun@gmail.com>
2023-01-03 10:39:33 +0100

    refactor: add extra debug statements in indexer

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.140-feat-one-search-dynamic-flow-20'
-NOW_PREPROCESSOR_VERSION = '0.0.118-feat-one-search-dynamic-flow-20'
-NOW_QDRANT_INDEXER_VERSION = '0.0.138-feat-one-search-dynamic-flow-20'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.139-feat-one-search-dynamic-flow-20'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.140-feat-one-search-dynamic-flow-21'
+NOW_PREPROCESSOR_VERSION = '0.0.118-feat-one-search-dynamic-flow-21'
+NOW_QDRANT_INDEXER_VERSION = '0.0.138-feat-one-search-dynamic-flow-21'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.139-feat-one-search-dynamic-flow-21'
 NOW_AUTOCOMPLETE_VERSION = '0.0.7-create-mm-docs-in-bff-18'
 
 
