530173d585800408d7ca7169357091c758833342
Florian Hönicke <hoenicke.florian@gmail.com>
2023-01-05 10:22:59 +0100

    refactor: dont use index method

--- tests/integration/offline_flow/test_offline_flow.py
+++ tests/integration/offline_flow/test_offline_flow.py
@@ -1,15 +1,18 @@
+import base64
 import random
-from typing import Dict
+from typing import Dict, Text
 
 import numpy as np
-from docarray import DocumentArray
+from docarray import Document, DocumentArray, dataclass
+from docarray.typing import Image
 
-from deployment.bff.app.v1.models.search import IndexRequestModel, SearchRequestModel
+from deployment.bff.app.v1.models.search import SearchRequestModel
 from deployment.bff.app.v1.routers import helper
-from deployment.bff.app.v1.routers.search import index, search
+from deployment.bff.app.v1.routers.search import search
 from now.executor.autocomplete import NOWAutoCompleteExecutor2
 from now.executor.indexer.elastic import NOWElasticIndexer
 from now.executor.preprocessor import NOWPreprocessor
+from now.now_dataclasses import UserInput
 
 
 def test_offline_flow(monkeypatch, setup_service_running, base64_image_string):
@@ -22,19 +25,34 @@ def test_offline_flow(monkeypatch, setup_service_running, base64_image_string):
     offline_client = get_client(offline_flow)
     monkeypatch.setattr(helper, 'get_jina_client', lambda **kwargs: offline_client)
 
-    index_result = index(
-        IndexRequestModel(
-            data=[
-                (
-                    {
-                        'product_title': {'text': 'test'},
-                        'product_image': {'blob': base64_image_string},
-                    },
-                    {'price': '4.50'},
-                )
-            ],
-        )
+    user_input = UserInput()
+    user_input.index_fields = ['product_title', 'product_description', 'product_image']
+
+    @dataclass
+    class Product:
+        product_title: Text
+        product_image: Image
+        product_description: Text
+
+    tensor = (
+        Document(blob=base64.b64decode(base64_image_string))
+        .convert_blob_to_image_tensor()
+        .tensor
     )
+    product = Product(
+        product_title='fancy title',
+        product_image=tensor,
+        product_description='this is a product',
+    )
+    index_result = offline_flow.post(
+        '/index',
+        inputs=DocumentArray(Document(product)),
+        parameters={
+            'access_paths': '@cc',
+            'user_input': user_input.__dict__,
+        },
+    )
+
     assert index_result == []
     search_result = search(
         SearchRequestModel(
