ade8264fab57df47e441c56d395e508312c14fce
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-01-06 19:41:04 +0100

    fix: removed multi-modal dependency

--- now/data_loading/data_loading.py
+++ now/data_loading/data_loading.py
@@ -59,12 +59,6 @@ def _field_dict_to_mm_doc(
     :param field_names_to_dataclass_fields: mapping of field names to data class fields (e.g. {'title': 'text_0'})
     :return: multi-modal document
     """
-    if len(field_dict) != 1:
-        raise ValueError(
-            f"Multi-modal document isn't supported yet. "
-            f"Can only set one value but have {list(field_dict.keys())}"
-        )
-
     with TemporaryDirectory() as tmp_dir:
         try:
             if field_names_to_dataclass_fields:
