45d17032c06b7843008cd80eeec05b93fd86d300
Joschka Braun <joschka.braun@gmail.com>
2023-01-06 12:23:12 +0100

    fix: fix typo

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.142-feat-one-search-dynamic-flow-60'
-NOW_PREPROCESSOR_VERSION = '0.0.119-feat-one-search-dynamic-flow-60'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.141-feat-one-search-dynamic-flow-60'
-NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-one-search-dynamic-flow-60'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.142-feat-one-search-dynamic-flow-61'
+NOW_PREPROCESSOR_VERSION = '0.0.119-feat-one-search-dynamic-flow-61'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.141-feat-one-search-dynamic-flow-61'
+NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-one-search-dynamic-flow-61'
 
 
 class Apps(BetterEnum):
