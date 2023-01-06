5771cbd3325a3c9d83b35bb9bd5493c6ef9634d8
Joschka Braun <joschka.braun@gmail.com>
2023-01-06 13:03:23 +0100

    chore: revert bff tag

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.143-feat-one-search-dynamic-flow-61'
-NOW_PREPROCESSOR_VERSION = '0.0.119-feat-one-search-dynamic-flow-61'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.141-feat-one-search-dynamic-flow-61'
-NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-one-search-dynamic-flow-61'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.143-mmdocs-in-playground-results-3'
+NOW_PREPROCESSOR_VERSION = '0.0.119-feat-one-search-dynamic-flow-62'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.141-feat-one-search-dynamic-flow-62'
+NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-one-search-dynamic-flow-62'
 
 
 class Apps(BetterEnum):
