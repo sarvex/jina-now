124f90894604e3e4811a7919776e8dc43c6c121e
jupyterjazz <saba.sturua@jina.ai>
2023-01-09 14:37:27 +0100

    refactor: change folder structure

--- now/common/testing.py
+++ now/common/testing.py
@@ -6,16 +6,19 @@ from now.constants import EXECUTOR_PREFIX
 def handle_test_mode(config):
     if os.environ.get('NOW_TESTING', False):
         from now.executor.autocomplete import NOWAutoCompleteExecutor2
+        from now.executor.indexer import NOWElasticIndexer
         from now.executor.preprocessor import NOWPreprocessor
 
         # this is a hack to make sure the import is not removed
-        if NOWPreprocessor and NOWAutoCompleteExecutor2:
+        if NOWPreprocessor and NOWAutoCompleteExecutor2 and NOWElasticIndexer:
             pass
 
         for k, v in config.items():
             if not (isinstance(v, str) and v.startswith(EXECUTOR_PREFIX)):
                 continue
             # replace only those executor config values which you want to locally test
+            if k == 'INDEXER_NAME':
+                config[k] = 'NOWElasticIndexer'
             elif k == 'AUTOCOMPLETE_EXECUTOR_NAME':
                 config[k] = 'NOWAutoCompleteExecutor2'
             elif k == 'PREPROCESSOR_NAME':
