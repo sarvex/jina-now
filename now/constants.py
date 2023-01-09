c4a8e968c8ed4144e5ab3d7946e11d6ea71c966d
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-01-09 17:31:46 +0100

    fix: updated versions

--- now/constants.py
+++ now/constants.py
@@ -5,9 +5,9 @@ from docarray.typing import Image, Text, Video
 from now.utils import BetterEnum
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.144-fix-bucket-uri-json-field-24'
-NOW_PREPROCESSOR_VERSION = '0.0.120-fix-bucket-uri-json-field-24'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.142-fix-bucket-uri-json-field-24'
-NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-one-search-dynamic-flow-66'
+NOW_PREPROCESSOR_VERSION = '0.0.120-fix-filter-index-fields-0'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.142-fix-filter-index-fields-0'
+NOW_AUTOCOMPLETE_VERSION = '0.0.8-fix-filter-index-fields-0'
 
 
 class Apps(BetterEnum):
