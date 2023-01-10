69a4d2dd7b81c3532b633a016de163dd00016256
jupyterjazz <saba.sturua@jina.ai>
2023-01-10 11:06:59 +0100

    refactor: enable removing lcl lcstr

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.145-refactor-bump-jina-version-to-latest-12'
-NOW_PREPROCESSOR_VERSION = '0.0.120-refactor-bump-jina-version-to-latest-12'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.142-refactor-bump-jina-version-to-latest-12'
-NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-bump-jina-version-to-latest-12'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.145-refactor-bump-jina-version-to-latest-13'
+NOW_PREPROCESSOR_VERSION = '0.0.120-refactor-bump-jina-version-to-latest-13'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.142-refactor-bump-jina-version-to-latest-13'
+NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-bump-jina-version-to-latest-13'
 
 
 class Apps(BetterEnum):
