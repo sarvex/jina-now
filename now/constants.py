8e22c1610fd5edd352faa446dccaf9df841e5ce4
jupyterjazz <saba.sturua@jina.ai>
2023-01-10 15:13:29 +0100

    chore: update streamlit version

--- now/constants.py
+++ now/constants.py
@@ -4,7 +4,7 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-fix-webcam-not-working'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-fix-webcam-not-working-1'
 NOW_PREPROCESSOR_VERSION = '0.0.120-fix-tag-filters-2'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.142-fix-tag-filters-2'
 NOW_AUTOCOMPLETE_VERSION = '0.0.8-feat-fix-tag-filters-2'
