4a6f0a432d5c3e5c22622af76f8c7f536cd9f275
jupyterjazz <saba.sturua@jina.ai>
2023-01-10 16:05:09 +0100

    chore: if this doesnt work idk wtd

--- now/constants.py
+++ now/constants.py
@@ -4,7 +4,7 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-fix-webcam-not-working-3'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-fix-webcam-not-working-4'
 NOW_PREPROCESSOR_VERSION = '0.0.120-fix-pull-docarray-4'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.142-fix-pull-docarray-4'
 NOW_AUTOCOMPLETE_VERSION = '0.0.8-fix-pull-docarray-4'
