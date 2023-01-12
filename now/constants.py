81491497ca3dad657b870d575caedd14c5e57f23
Florian Hönicke <hoenicke.florian@gmail.com>
2023-01-12 13:05:58 +0100

    refactor: bump jina version (#862)
    * refactor: bump jina version
    
    * fix: version
    
    * test: experimenting with local tests
    
    * refactor: bump bff req
    
    * refactor: revert tests
    
    * refactor: fix indentation
    
    * refactor: disable removing local cluster
    
    * refactor: enable removing lcl lcstr
    
    * refactor: remove tmate sessio
    
    Co-authored-by: jupyterjazz <saba.sturua@jina.ai>
    Co-authored-by: Saba Sturua <45267439+jupyterjazz@users.noreply.github.com>

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-refactor-user-input-in-args-21'
-NOW_PREPROCESSOR_VERSION = '0.0.120-refactor-user-input-in-args-21'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.142-refactor-update-mapping-3'
-NOW_AUTOCOMPLETE_VERSION = '0.0.9-refactor-user-input-in-args-21'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-refactor-bump-jina-version-to-latest-17'
+NOW_PREPROCESSOR_VERSION = '0.0.120-refactor-bump-jina-version-to-latest-17'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.142-refactor-bump-jina-version-to-latest-17'
+NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-bump-jina-version-to-latest-17'
 
 
 class Apps(BetterEnum):
