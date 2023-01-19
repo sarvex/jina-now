30a0d58a63f84c7987acb0385819b2aeb2d90f13
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-01-19 09:29:28 +0100

    fix: update tags

--- now/constants.py
+++ now/constants.py
@@ -4,11 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-fix-filter-index-fields-12'
-NOW_PREPROCESSOR_VERSION = '0.0.120-fix-filter-index-fields-12'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.142-fix-filter-index-fields-12'
-NOW_AUTOCOMPLETE_VERSION = '0.0.9-fix-filter-index-fields-12'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-fix-filter-index-fields-13'
+NOW_PREPROCESSOR_VERSION = '0.0.120-fix-filter-index-fields-13'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.142-fix-filter-index-fields-13'
+NOW_AUTOCOMPLETE_VERSION = '0.0.9-fix-filter-index-fields-13'
 
 
 class Apps(BetterEnum):
