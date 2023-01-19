29c4329c9824272f97c9ad655889b69b8e20bc7d
Florian Hönicke <hoenicke.florian@gmail.com>
2023-01-19 20:44:38 +0100

    Fix playground import (#956)
    * fix: playground import
    
    * fix: playground

--- now/constants.py
+++ now/constants.py
@@ -4,8 +4,7 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.147-refactor-remove-k8s-9'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.147-fix-playground-import-2'
 NOW_PREPROCESSOR_VERSION = '0.0.121-refactor-remove-ocr-4'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.143-refactor-remove-ocr-4'
 NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-remove-k8s-9'
