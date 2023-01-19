b8b4b96b1368f7bfc1fb2f3fd7faad221c526ca6
Florian Hönicke <hoenicke.florian@gmail.com>
2023-01-19 23:00:17 +0100

    fix: skip

--- now/constants.py
+++ now/constants.py
@@ -4,7 +4,7 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.148-refactor-clean-up'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.148-refactor-clean-up-1'
 NOW_PREPROCESSOR_VERSION = '0.0.121-refactor-remove-ocr-4'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.144-test-optimize-tests-4'
 NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-remove-k8s-9'
