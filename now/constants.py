6ffdd51631191d86fe4902f48663a1063418153b
Mohammad Kalim Akram <kalim.akram@jina.ai>
2023-01-20 10:42:05 +0100

    fix: trigger example deployment

--- now/constants.py
+++ now/constants.py
@@ -5,7 +5,7 @@ from docarray.typing import Image, Text, Video
 from now.utils import BetterEnum
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.148-refactor-clean-up-1'
-NOW_PREPROCESSOR_VERSION = '0.0.122-demo-examples-1'
+NOW_PREPROCESSOR_VERSION = '0.0.122-demo-examples-0'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.144-test-optimize-tests-4'
 NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-remove-k8s-9'
 
