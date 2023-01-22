102c7cf85c4517e5916d874ab188d4b05067a50d
Mohammad Kalim Akram <kalim.akram@jina.ai>
2023-01-22 21:26:10 +0100

    fix: update tag

--- now/constants.py
+++ now/constants.py
@@ -5,7 +5,7 @@ from docarray.typing import Image, Text, Video
 from now.utils import BetterEnum
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.148-refactor-remove-now-testing-4'
-NOW_PREPROCESSOR_VERSION = '0.0.123-demo-examples-0'
+NOW_PREPROCESSOR_VERSION = '0.0.123-demo-examples-1'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.144-refactor-remove-now-testing-4'
 NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-remove-now-testing-4'
 
