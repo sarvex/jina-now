33423db9a38137cf49eadd72203c08b48828b76f
azayz <azizbellaweid@hotmail.com>
2023-01-25 11:12:46 +0100

    fix: debug

--- now/executor/indexer/elastic/elastic_indexer.py
+++ now/executor/indexer/elastic/elastic_indexer.py
@@ -445,7 +445,9 @@ class NOWElasticIndexer(Executor):
                 # aggs['aggs'][f'avg_{tag}'] = {'avg': {'field': f'tags.{tag}'}}
                 aggs['aggs'][tag] = {'terms': {'field': f'tags.{tag}', 'size': 100}}
         try:
+            print('aggs', aggs)
             result = self.es.search(index=self.index_name, body=aggs)
+            print('result', result)
             aggregations = result['aggregations']
             updated_tags = {}
             for tag, agg in aggregations.items():
