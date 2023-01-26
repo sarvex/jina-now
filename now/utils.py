4181ee1c7180d73938829428d7e11d426dcc9e0b
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-01-26 14:29:26 +0100

    fix: pr comments

--- now/utils.py
+++ now/utils.py
@@ -20,6 +20,7 @@ from docarray import Document, DocumentArray
 from jina.jaml import JAML
 from pyfiglet import Figlet
 
+from now.now_dataclasses import UserInput
 from now.thirdparty.PyInquirer.prompt import prompt
 
 
@@ -332,13 +333,15 @@ def get_email():
         return ''
 
 
-def list_s3_objects(bucket_name, prefix, user_input, max_keys=1000):
+def list_s3_objects(
+    bucket_name: str, prefix: str, user_input: UserInput, max_keys: int = 1000
+):
     session = boto3.Session(
         aws_access_key_id=user_input.aws_access_key_id,
         aws_secret_access_key=user_input.aws_secret_access_key,
     )
 
-    client = session.client('s3', region_name='eu-west-1')
+    client = session.client('s3', region_name=user_input.aws_region_name)
     response = client.list_objects_v2(
         Bucket=bucket_name, Prefix=prefix, MaxKeys=max_keys
     )
