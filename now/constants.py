82f2201d54121e240ae5d1a941acd9e9a608cdd0
Florian Hönicke <hoenicke.florian@gmail.com>
2023-01-30 13:24:39 +0100

    fix: elastic persitent data

--- now/constants.py
+++ now/constants.py
@@ -6,7 +6,7 @@ from now.utils import BetterEnum
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.146-fix-filter-index-fields-20'
 NOW_PREPROCESSOR_VERSION = '0.0.123-demo-examples-5'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.142-fix-filter-index-fields-20'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.143-fix-elastic-persistent-workspace-1'
 NOW_AUTOCOMPLETE_VERSION = '0.0.9-fix-filter-index-fields-20'
 
 
