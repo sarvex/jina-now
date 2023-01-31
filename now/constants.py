02672487fb20604cace4dc0b86a5c7f3d9a1eab5
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-01-31 17:45:29 +0100

    fix: update tags

--- now/constants.py
+++ now/constants.py
@@ -5,7 +5,7 @@ from docarray.typing import Image, Text, Video
 from now.utils import BetterEnum
 
 DOCKER_BFF_PLAYGROUND_TAG = '0.0.152-bump-jcloud'
-NOW_PREPROCESSOR_VERSION = '0.0.120-fix-support-tags-for-folders-18'
+NOW_PREPROCESSOR_VERSION = '0.0.120-fix-support-tags-for-folders-19'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.144-bump-jcloud'
 NOW_AUTOCOMPLETE_VERSION = '0.0.10-bump-jcloud'
 
