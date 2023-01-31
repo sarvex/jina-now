2f6723c2bb94b48840f5b226b491b6551fb71246
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-01-31 14:55:58 +0100

    fix: update assertions

--- tests/integration/remote/assertions.py
+++ tests/integration/remote/assertions.py
@@ -156,7 +156,7 @@ def assert_search_custom_s3(host, create_temp_link=False):
     response_json = response.json()
     assert len(response_json) == 2
     for doc in response_json:
-        field = list(doc['fields'].values())[0]
+        field = list(doc['fields'].values())[1]
         if create_temp_link:
             assert not field['uri'].startswith('s3://'), f"received: {doc}"
         else:
