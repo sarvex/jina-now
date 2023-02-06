5422a428e4ea3f114264e365e7f265f9f8bd63f1
Isabelle Mohr <isabelle.mohr@jina.ai>
2023-02-06 13:02:12 +0100

    fix: use pip3 in docker

--- now/constants.py
+++ now/constants.py
@@ -4,9 +4,9 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-DOCKER_BFF_PLAYGROUND_TAG = '0.0.151-fix-sem-scores-19'
-NOW_PREPROCESSOR_VERSION = '0.0.120-fix-sem-scores-19'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.144-fix-sem-scores-19'
+DOCKER_BFF_PLAYGROUND_TAG = '0.0.151-fix-sem-scores-20'
+NOW_PREPROCESSOR_VERSION = '0.0.120-fix-sem-scores-20'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.144-fix-sem-scores-20'
 NOW_AUTOCOMPLETE_VERSION = '0.0.11-fix-av-requirements-2'
 
 
