eedffe3fa422c64697a361ccd73452de54b62a90
Tanguy Abel <tanguy.abel@gmail.com>
2023-02-07 16:15:40 +0100

    chore: update es indexer tag

--- now/constants.py
+++ now/constants.py
@@ -6,7 +6,7 @@ from now.utils import BetterEnum
 
 NOW_GATEWAY_VERSION = '0.0.1-refactor-s3-data-loading-9'
 NOW_PREPROCESSOR_VERSION = '0.0.122-refactor-custom-gateway-103'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.147-fix-elastic-persistent-workspace-9'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.147-fix-gif-truncated-1'
 NOW_AUTOCOMPLETE_VERSION = '0.0.11-refactor-custom-gateway-103'
 
 
