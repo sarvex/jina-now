5741b1906043a6bcd55ef98e35ba7e8ed428df97
Isabelle Mohr <isabelle.mohr@jina.ai>
2023-02-07 21:47:53 +0100

    refactor: remove debug prints in playground

--- now/executor/gateway/playground/playground.py
+++ now/executor/gateway/playground/playground.py
@@ -546,14 +546,14 @@ def render_multi_modal_result(match, c):
         render_graphic_result(chunk, c)
         render_text_result(chunk, c)
     if st.session_state.show_score_breakdown:
-        match.scores.pop('cosine', None)
-        semantic_names = list(match.scores.keys())
-        values = list(match.scores.values())
-        display_scores = "<br>".join(
+        total_score = match.scores.pop('cosine')
+        semantic_names = list(match.scores.keys()) + ['total score']
+        values = list(match.scores.values()) + [total_score]
+        display_scores_string = "<br>".join(
             name + ": " + str(round(v.value, 3))
             for name, v in zip(semantic_names, values)
         )
-        body = f"<!DOCTYPE html><html><body>{display_scores}</body></html>"
+        body = f'<!DOCTYPE html><html><body><p style = "font-size:9px;">{display_scores_string}</body></html>'
         c.markdown(
             body=body,
             unsafe_allow_html=True,
