ea3424e37c4567b8013e612ffe9c135543365081
Isabelle Mohr <isabelle.mohr@jina.ai>
2023-02-07 20:20:26 +0100

    fix: typo

--- now/executor/gateway/playground/playground.py
+++ now/executor/gateway/playground/playground.py
@@ -548,7 +548,7 @@ def render_multi_modal_result(match, c):
     if st.session_state.show_score_breakdown:
         match.scores.pop('cosine', None)
         semantic_names = list(match.scores.keys())
-        values = [v for _, v in list(match.scores.values())]
+        values = list(match.scores.values())
         display_scores = "<br>".join(
             name + " : " + str(value) for name, value in zip(semantic_names, values)
         )
