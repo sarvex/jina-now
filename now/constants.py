a068d82d62792f707fcca2777b9863208025b0d0
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-02-10 12:31:50 +0100

    fix: update versions

--- now/constants.py
+++ now/constants.py
@@ -4,9 +4,9 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.1-feat-score-results-13'
+NOW_GATEWAY_VERSION = '0.0.1-fix-conflicting-name-0'
 NOW_PREPROCESSOR_VERSION = '0.0.122-refactor-custom-gateway-103'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.147-feat-score-results-13'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.147-fix-conflicting-name-0'
 NOW_AUTOCOMPLETE_VERSION = '0.0.11-refactor-custom-gateway-103'
 
 
