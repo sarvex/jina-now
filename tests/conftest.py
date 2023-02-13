2fd3de66c22cd2eaeb41796728c0c624c3811178
jupyterjazz <saba.sturua@jina.ai>
2023-02-13 14:14:30 +0100

    refactor: fixture header

--- tests/conftest.py
+++ tests/conftest.py
@@ -323,5 +323,6 @@ def setup_online_shop_db(setup_elastic_db, es_connection_params, online_shop_res
     delete_es_index(connector=es_connector, name=index_name)
 
 
+@pytest.fixture
 def random_index_name():
     return f'test-index-{random.randint(0, 10000)}'
