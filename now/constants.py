1554dfa751eb0c0c09d9da3d495103d49f2fb5ea
Florian Hönicke <hoenicke.florian@gmail.com>
2023-02-15 17:57:25 +0100

    fix: constants

--- now/constants.py
+++ now/constants.py
@@ -6,7 +6,7 @@ from now.utils import BetterEnum
 
 NOW_GATEWAY_VERSION = '0.0.4-fix-index-bulk-error-9'
 NOW_PREPROCESSOR_VERSION = '0.0.123-test-use-setup-25'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.148-test-experiment-volume-7'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.148-test-experiment-volume-8'
 NOW_AUTOCOMPLETE_VERSION = '0.0.11-refactor-custom-gateway-103'
 
 
