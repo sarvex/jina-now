a2bd49065af993c5fc4d770913860cefeab5e1ce
Florian Hönicke <hoenicke.florian@gmail.com>
2023-02-28 00:06:37 +0100

    refactor: update all

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.4-fix-custom-gateway-remote-4'
-NOW_PREPROCESSOR_VERSION = '0.0.124-refactor-no-batchwise-indexing-2'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.147-feat-integrate-elastic-manager-17'
-NOW_AUTOCOMPLETE_VERSION = '0.0.11-refactor-custom-gateway-103'
+NOW_GATEWAY_VERSION = '0.0.5-refactor-external-clip-1'
+NOW_PREPROCESSOR_VERSION = '0.0.125-refactor-external-clip-1'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.148-refactor-external-clip-1'
+NOW_AUTOCOMPLETE_VERSION = '0.0.12-refactor-external-clip-1'
 
 
 class Apps(BetterEnum):
