6a2be45083f103db3952de90ef3bc89ff62bd286
Florian Hönicke <hoenicke.florian@gmail.com>
2023-02-28 14:48:03 +0100

    refactor: remove tests

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.5-refactor-external-clip-5'
-NOW_PREPROCESSOR_VERSION = '0.0.125-refactor-external-clip-5'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.148-refactor-external-clip-5'
-NOW_AUTOCOMPLETE_VERSION = '0.0.12-refactor-external-clip-5'
+NOW_GATEWAY_VERSION = '0.0.5-refactor-external-clip-6'
+NOW_PREPROCESSOR_VERSION = '0.0.125-refactor-external-clip-6'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.148-refactor-external-clip-6'
+NOW_AUTOCOMPLETE_VERSION = '0.0.12-refactor-external-clip-6'
 
 
 class Apps(BetterEnum):
@@ -53,7 +53,7 @@ BASE_STORAGE_URL = (
 )
 
 EXTERNAL_CLIP_HOST = 'encoderclip-pretty-javelin-3aceb7f2cd.wolf.jina.ai'
-EXTERNAL_SBERT_HOST = 'flow-external-sbert-73169ff019.wolf.jina.ai'
+EXTERNAL_SBERT_HOST = 'flow-external-sbert-0f5043298f.wolf.jina.ai'
 
 DEFAULT_FLOW_NAME = 'nowapi'
 PREFETCH_NR = 10
--- tests/integration/remote/test_e2e_docarray.py
+++ tests/integration/remote/test_e2e_docarray.py
@@ -20,25 +20,6 @@ from now.demo_data import DemoDatasetNames
     'query_fields, index_fields, filter_fields, model_selection, dataset',
     [
         (
-            'image',
-            ['image', 'label'],
-            [],
-            {
-                'image_model': [Models.CLIP_MODEL],
-                'label_model': [Models.CLIP_MODEL, Models.SBERT_MODEL],
-            },
-            DemoDatasetNames.BIRD_SPECIES,
-        ),
-        (
-            'image',
-            ['image'],
-            ['label'],
-            {
-                'image_model': [Models.CLIP_MODEL],
-            },
-            DemoDatasetNames.BIRD_SPECIES,
-        ),
-        (
             'text',
             ['lyrics'],
             [],
@@ -54,15 +35,6 @@ from now.demo_data import DemoDatasetNames
                 'description_model': [Models.CLIP_MODEL],
             },
             DemoDatasetNames.TUMBLR_GIFS_10K,
-        ),
-        (
-            'text',
-            ['image'],
-            ['label'],
-            {
-                'image_model': [Models.CLIP_MODEL],
-            },
-            DemoDatasetNames.BEST_ARTWORKS,
         ),
     ],
 )
