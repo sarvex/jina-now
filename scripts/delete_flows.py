5c531130639de1e872ce974bf3497f662c681dcb
jupyterjazz <saba.sturua@jina.ai>
2023-03-01 14:06:08 +0100

    refactor: remove local setup

--- scripts/delete_flows.py
+++ scripts/delete_flows.py
@@ -19,13 +19,15 @@ def delete_ci_flows():
     and look for those who have the current branch name inside their name, and delete only those.
     """
     print('Deleting the flows that were created during this CI run')
-    branch_name = os.environ.get('GITHUB_HEAD_REF', 'local_setup').lower()[:15]
-    loop = create_eventloop()
-    jflows = loop.run_until_complete(CloudFlow().list_all())['flows']
-    for flow in jflows:
-        if flow['status']['phase'] != 'Deleted' and branch_name in flow['id']:
-            CloudFlow(flow_id=flow['id']).__exit__()
-            print(f"flow {flow['id']} is successfully terminated")
+    branch_name = os.environ.get('GITHUB_HEAD_REF')
+    if branch_name:
+        branch_name = branch_name.lower()[:15]
+        loop = create_eventloop()
+        jflows = loop.run_until_complete(CloudFlow().list_all())['flows']
+        for flow in jflows:
+            if flow['status']['phase'] != 'Deleted' and branch_name in flow['id']:
+                CloudFlow(flow_id=flow['id']).__exit__()
+                print(f"flow {flow['id']} is successfully terminated")
     print('Flows are cleaned up')
 
 
