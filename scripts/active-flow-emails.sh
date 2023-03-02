07a4945868a84fb3b0e4ee06a664821b2c110088
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-03-02 13:22:38 +0100

    feat: add python script

--- scripts/active-flow-emails.sh
+++ scripts/active-flow-emails.sh
@@ -1,14 +1,19 @@
-kubectl get pods -A | grep jnamespace | awk '{print $1}' | uniq -c | sort -nr | while read name; do
-  num_flows=$(echo "$name" | cut -d' ' -f1)
+#!/bin/sh
+
+results=()
+kubectl get pods -A | grep jnamespace | awk '{print $1}' | uniq -c | sort -nr | head -n 5 | while read name; do
+  num_executors=$(echo "$name" | cut -d' ' -f1)
   namespace=$(echo "$name" | cut -d' ' -f2)
   flow_id=$(echo "$name" | cut -d- -f2-2)
-  echo $namespace
   user_id=`kubectl get flow -n $namespace -o 'jsonpath={.items[*].metadata.labels.jina\.ai\/user}'`
-  user_email=$(curl -X POST \
+  user_email=$(curl -s -X POST \
     -H "Content-Type: application/json" \
-    -H "Authorization: $AUTH_TOKEN" \
+    -H "Authorization: TOKEN" \
     -d '{"ids": ["'${user_id}'"]}' "https://api.hubble.jina.ai/v2/rpc/user.m2m.listUserInfo" \
     | jq -r '.data[0].email')
-
-    echo "$user_email has $num_flows active executors in the flow ID $flow_id"
+    result="$user_email has $num_executors active executors in the flow ID $flow_id"
+    echo "$user_email, $num_executors, $flow_id"
+    results+=("$result")
 done
+
+echo "${results}"
--- scripts/alert_slack_with_flows.py
+++ scripts/alert_slack_with_flows.py
@@ -1,12 +1,44 @@
+import subprocess
+from collections import defaultdict
+
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 client = WebClient(token="TOKEN")
 
-# output = subprocess.check_output(["./active-flow-emails.sh"], universal_newlines=True)
+output = subprocess.check_output(["./active-flow-emails.sh"], universal_newlines=True)
+
+lines = list(filter(None, output.split('\n')))
+users = ""
+dct_executors = defaultdict(int)
+dct_ids = defaultdict(list)
+for line in lines:
+    email, num_executors, flow_id = line.split(', ')
+    dct_executors[email] += int(num_executors)
+    dct_ids[email].append(flow_id)
+
+for email, executor_count in sorted(
+    dct_executors.items(), key=lambda kv: kv[1], reverse=True
+):
+    users += f"- {email} has {executor_count} active executors in the flow IDs {', '.join(dct_ids[email])}\n"
+
+message = {
+    "channel": "#slack-function-test",
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*Hello* 👋 below is the list of users with most active executors, please make sure to clean them up!",
+            },
+        },
+        {"type": "divider"},
+        {"type": "section", "fields": [{"type": "mrkdwn", "text": users}]},
+    ],
+}
 
 try:
-    response = client.chat_postMessage(channel="#zac-test", text='test')
+    response = client.chat_postMessage(**message)
     print("Message sent: ", response["ts"])
 except SlackApiError as e:
     print("Error sending message: ", e)
