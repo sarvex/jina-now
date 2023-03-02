1a33d146112c59698518cad886368b8b8cfea6bb
Saba Sturua <45267439+jupyterjazz@users.noreply.github.com>
2023-03-02 13:59:58 +0100

    chore: bump jcloud version (#1143)
    * chore: bump jcloud version
    
    * chore: update constants

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.4-fix-use-spot-0'
-NOW_PREPROCESSOR_VERSION = '0.0.124-fix-use-spot-0'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.148-fix-use-spot-0'
-NOW_AUTOCOMPLETE_VERSION = '0.0.11-feat-autoscale-suggestion-11'
+NOW_GATEWAY_VERSION = '0.0.5-bump-jcloud'
+NOW_PREPROCESSOR_VERSION = '0.0.125-bump-jcloud'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.149-bump-jcloud'
+NOW_AUTOCOMPLETE_VERSION = '0.0.12-bump-jcloud'
 
 
 class Apps(BetterEnum):
