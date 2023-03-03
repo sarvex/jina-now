9e8b7e99d1d42630502e90bdd23e83fb566b4b65
IyadhKhalfallah <35614094+IyadhKhalfallah@users.noreply.github.com>
2023-03-03 11:11:57 +0100

    feat: add active flow emails to slack (#1139)
    * feat: add script to get active flows emails
    
    * fix: add eof space
    
    * feat: add slack message script
    
    * feat: add python script
    
    * feat: add action
    
    * fix: add requirements
    
    * fix: directly install slack sdk
    
    * fix: python env
    
    * fix: file path
    
    * fix: add ls
    
    * fix: add ls
    
    * fix: add flags
    
    * fix: file path
    
    * fix: remove extra slash
    
    * fix: sh
    
    * fix: add ls
    
    * fix: add echo
    
    * fix: change syntax
    
    * fix: remove extra slash
    
    * fix: test sh
    
    * fix: remove extra lines from script
    
    * fix: print message
    
    * fix: print script output
    
    * fix: add echo
    
    * fix: add aws support
    
    * fix: update token
    
    * fix: added echo name
    
    * fix: removed echo
    
    * fix: removed echo
    
    * feat: add cron
    
    * fix: update tags

---
+++ scripts/active-flow-emails.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+token="$1"
+kubectl get pods -A | grep jnamespace | awk '{print $1}' | uniq -c | sort -nr | head -n 5 | while read name; do
+  num_executors=$(echo "$name" | cut -d' ' -f1)
+  namespace=$(echo "$name" | cut -d' ' -f2)
+  flow_id=$(echo "$name" | cut -d- -f2-2)
+  user_id=`kubectl get flow -n $namespace -o 'jsonpath={.items[*].metadata.labels.jina\.ai\/user}'`
+  user_email=$(curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Authorization: $token" \
+    -d '{"ids": ["'${user_id}'"]}' "https://api.hubble.jina.ai/v2/rpc/user.m2m.listUserInfo" \
+    | jq -r '.data[0].email')
+    echo "$user_email, $num_executors, $flow_id"
+done
