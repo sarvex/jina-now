69f48766dd849d648db659b66f5e44207ec9e004
azayz <azizbellaweid@hotmail.com>
2023-03-07 12:04:30 +0100

    fix: black

--- now/constants.py
+++ now/constants.py
@@ -4,11 +4,11 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-
 NOW_GATEWAY_VERSION = '0.0.4-feat-change-auth-http-82'
 NOW_PREPROCESSOR_VERSION = '0.0.123-feat-integrate-billing-92'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.147-feat-integrate-billing-92'
 NOW_AUTOCOMPLETE_VERSION = '0.0.11-feat-integrate-billing-92'
+
 
 class Apps(BetterEnum):
     SEARCH_APP = 'search_app'
