f60ea2d5c89accc699dd9fef461923017dcd0b3b
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-03-09 11:17:05 +0100

    fix: remove extra function

--- now/utils/flow/helpers.py
+++ now/utils/flow/helpers.py
@@ -1,36 +1,5 @@
-import cowsay
 import yaml
 from jina.jaml import JAML
-
-from now.deployment.deployment import list_all_wolf, status_wolf
-
-
-def get_flow_status(action, **kwargs):
-    choices = []
-    # Add all remote Flows that exists with the namespace `nowapi`
-    alive_flows = list_all_wolf(status='Serving')
-    for flow_details in alive_flows:
-        choices.append(flow_details['name'])
-    if len(choices) == 0:
-        cowsay.cow(f'nothing to {action}')
-        return
-    else:
-        questions = [
-            {
-                'type': 'list',
-                'name': 'cluster',
-                'message': f'Which cluster do you want to {action}?',
-                'choices': choices,
-            }
-        ]
-        cluster = maybe_prompt_user(questions, 'cluster', **kwargs)
-
-    flow = [x for x in alive_flows if x['name'] == cluster][0]
-    flow_id = flow['id']
-    _result = status_wolf(flow_id)
-    if _result is None:
-        print(f'❎ Flow not found in JCloud. Likely, it has been deleted already')
-    return _result, flow_id, cluster
 
 
 def get_flow_id(host):
