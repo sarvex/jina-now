c8807dd32e38be007ebeb593861ea13b581f64df
Mohammad Kalim Akram <kalim.akram@jina.ai>
2023-03-09 14:57:54 +0100

    fix: increase timeout
    Signed-off-by: Mohammad Kalim Akram <kalim.akram@jina.ai>

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.6-fix-test-timeout-23'
-NOW_PREPROCESSOR_VERSION = '0.0.125-fix-test-timeout-23'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.149-fix-test-timeout-23'
-NOW_AUTOCOMPLETE_VERSION = '0.0.12-fix-test-timeout-23'
+NOW_GATEWAY_VERSION = '0.0.6-fix-test-timeout-24'
+NOW_PREPROCESSOR_VERSION = '0.0.125-fix-test-timeout-24'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.149-fix-test-timeout-24'
+NOW_AUTOCOMPLETE_VERSION = '0.0.12-fix-test-timeout-24'
 
 
 class Apps(BetterEnum):
