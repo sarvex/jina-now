ce5d72d01d845fb649716192b47c1f96e32bd6b7
azayz <azizbellaweid@hotmail.com>
2023-03-15 14:59:45 +0100

    fix: test

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -6,16 +6,20 @@ from now.client import Client
 from now.executor.gateway.bff.app.v1.routers import helper
 
 
-def test_send_request_bff(requests_mock):
-    requests_mock.post(
-        'https://nowrun.jina.ai/api/v1/search', json={'status_code': 200}
-    )
-
-    client = Client(
+@pytest.fixture
+def client():
+    return Client(
         jcloud_id='jcloud_id',
         app='app',
         api_key='api_key',
     )
+
+
+def test_send_request_bff(client, requests_mock):
+    requests_mock.post(
+        'https://nowrun.jina.ai/api/v1/search', json={'status_code': 200}
+    )
+
     response = client.send_request_bff(
         endpoint='search',
         text='text',
@@ -24,21 +28,25 @@ def test_send_request_bff(requests_mock):
 
 
 @pytest.mark.asyncio
-async def test_send_request_not_search_endpoint():
+async def test_send_request_not_search_endpoint(client):
     with pytest.raises(NotImplementedError):
-        client = Client(
-            jcloud_id='jcloud_id',
-            app='app',
-            api_key='api_key',
-        )
+
         await client.send_request(
             endpoint='not_search',
             text='text',
         )
 
 
+async def test_send_request_no_query_doc(client):
+    with pytest.raises(Exception):
+
+        await client.send_request(
+            endpoint='not_search',
+        )
+
+
 @pytest.mark.asyncio
-async def test_send_requests(mocker):
+async def test_send_requests(mocker, client):
     mock_async_func = MagicMock()
 
     async def async_mock_async_func(endpoint, text, *args, **kwargs):
@@ -48,11 +56,6 @@ async def test_send_requests(mocker):
 
     helper.jina_client_post = async_mock_async_func
 
-    client = Client(
-        jcloud_id='jcloud_id',
-        app='app',
-        api_key='api_key',
-    )
     response = await client.send_request(
         endpoint='search',
         text='text',
