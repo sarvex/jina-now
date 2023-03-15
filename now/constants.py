064cda3636b202b7c518a6924311c9300a4a614b
Mohammad Kalim Akram <kalim.akram@jina.ai>
2023-03-15 18:12:02 +0100

    fix: tests
    Signed-off-by: Mohammad Kalim Akram <kalim.akram@jina.ai>

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils.common.helpers import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.6-fix-m2m-token-9'
-NOW_PREPROCESSOR_VERSION = '0.0.125-fix-m2m-token-9'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.149-fix-m2m-token-9'
-NOW_AUTOCOMPLETE_VERSION = '0.0.12-fix-m2m-token-9'
+NOW_GATEWAY_VERSION = '0.0.6-fix-m2m-token-10'
+NOW_PREPROCESSOR_VERSION = '0.0.125-fix-m2m-token-10'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.149-fix-m2m-token-10'
+NOW_AUTOCOMPLETE_VERSION = '0.0.12-fix-m2m-token-10'
 
 
 class Apps(BetterEnum):
