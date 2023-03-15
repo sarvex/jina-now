b14a404bed6999455f6b329cb025922b5513be25
Joschka Braun <joschka.braun@gmail.com>
2023-03-15 10:38:50 +0100

    fix: fix deactivate autoscale

--- now/app/search_app/app.py
+++ now/app/search_app/app.py
@@ -75,12 +75,12 @@ class SearchApp(JinaNOWApp):
             'needs': 'gateway',
             'env': {'JINA_LOG_LEVEL': 'DEBUG'},
             'jcloud': {
-                'autoscale': {
-                    'min': 0,
-                    'max': 1,
-                    'metric': 'concurrency',
-                    'target': 1,
-                },
+                # 'autoscale': {
+                #     'min': 0,
+                #     'max': 1,
+                #     'metric': 'concurrency',
+                #     'target': 1,
+                # },
                 'resources': {
                     'instance': 'C1',
                     'capacity': 'spot',
@@ -98,12 +98,12 @@ class SearchApp(JinaNOWApp):
             if not testing
             else 'NOWPreprocessor',
             'jcloud': {
-                'autoscale': {
-                    'min': 0,
-                    'max': 100,
-                    'metric': 'concurrency',
-                    'target': 1,
-                },
+                # 'autoscale': {
+                #     'min': 0,
+                #     'max': 100,
+                #     'metric': 'concurrency',
+                #     'target': 1,
+                # },
                 'resources': {'instance': 'C4', 'capacity': 'spot'},
             },
             'env': {'JINA_LOG_LEVEL': 'DEBUG'},
