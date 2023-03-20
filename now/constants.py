04c4c4de641d105090bd6e42fb2879f6da4004da
IyadhKhalfallah <iyadhkhalfallah98@gmail.com>
2023-03-20 13:18:04 +0100

    fix: update tags

--- now/constants.py
+++ now/constants.py
@@ -4,7 +4,7 @@ from docarray.typing import Image, Text, Video
 
 from now.utils.common.helpers import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.6-fix-m2m-token-29'
+NOW_GATEWAY_VERSION = '0.0.6-fix-cli-coverage-0'
 NOW_PREPROCESSOR_VERSION = '0.0.125-fix-m2m-token-29'
 NOW_ELASTIC_INDEXER_VERSION = '0.0.149-fix-m2m-token-29'
 NOW_AUTOCOMPLETE_VERSION = '0.0.12-fix-m2m-token-29'
