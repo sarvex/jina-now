704ae2a410cc2fcaf4bfcd0db16dad7d3c9d4945
Joschka Braun <joschka.braun@gmail.com>
2023-03-21 12:14:20 +0100

    chore: bump version

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils.common.helpers import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.6-fix-m2m-token-29'
-NOW_PREPROCESSOR_VERSION = '0.0.125-feat-weekly-latency-1'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.149-fix-m2m-token-29'
-NOW_AUTOCOMPLETE_VERSION = '0.0.12-fix-m2m-token-29'
+NOW_GATEWAY_VERSION = '0.0.6-feat-weekly-latency-2'
+NOW_PREPROCESSOR_VERSION = '0.0.125-feat-weekly-latency-2'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.149-feat-weekly-latency-2'
+NOW_AUTOCOMPLETE_VERSION = '0.0.12-feat-weekly-latency-2'
 
 
 class Apps(BetterEnum):
